401480e983f22f275ed807ee0ba7477454a61bfe
ChenHengming <ChenHengming@6e48237b-75ef-9749-8fc9-41990f28c85a>
2015-07-27 01:37:10 +0000

    [inc]: add rk_type.h
    git-svn-id: https://10.10.10.66:8443/svn/MediaProcessPlatform/trunk/mpp@2 6e48237b-75ef-9749-8fc9-41990f28c85a

---
+++ inc/rk_type.h
@@ -0,0 +1,59 @@
+/*
+ *
+ * Copyright 2010 Rockchip Electronics S.LSI Co. LTD
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * File         : rk_type.h
+ * Description  : type definition
+ * Author       : Jian Huan
+ * Date         : 2010-11-23 21:48:40
+ */
+ 
+#ifndef _RK_TYPE_H_
+#define _RK_TYPE_H_
+
+#if defined(_WIN32) && !defined(__MINGW32CE__)
+
+typedef unsigned char           RK_U8;
+typedef unsigned short          RK_U16;
+typedef unsigned int            RK_U32;
+typedef unsigned __int64        RK_U64;
+
+typedef signed char             RK_S8;
+typedef signed short            RK_S16;
+typedef signed int              RK_S32;
+typedef signed __int64          RK_S64;
+
+typedef void                    VOID;
+
+#else
+
+typedef unsigned char           RK_U8;
+typedef unsigned short          RK_U16;
+typedef unsigned int            RK_U32;
+typedef unsigned long long int  RK_U64;
+
+
+typedef signed char             RK_S8;
+typedef signed short            RK_S16;
+typedef signed int              RK_S32;
+typedef signed long long int    RK_S64;
+
+typedef void                    VOID;
+
+#endif
+
+#endif /*_RK_TYPE_H_*/
