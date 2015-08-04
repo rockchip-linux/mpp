cd7f86c83af575707a2033a1926946a3524674d8
ChenHengming <ChenHengming@6e48237b-75ef-9749-8fc9-41990f28c85a>
2015-08-04 07:26:24 +0000

    [cmake/android]: add build script on Android
    git-svn-id: https://10.10.10.66:8443/svn/MediaProcessPlatform/trunk/mpp@25 6e48237b-75ef-9749-8fc9-41990f28c85a

---
+++ build/android/make-Android.bash
@@ -0,0 +1,40 @@
+#!/bin/bash
+# Run this from within a bash shell
+ANDROID_NDK=~/work/android/ndk/android-ndk-r10d/
+PLATFORM=$ANDROID_NDK/platforms/android-17/arch-arm
+
+cmake -DCMAKE_TOOLCHAIN_FILE=./android.toolchain.cmake                      \
+      -DANDROID_NDK=${ANDROID_NDK}                                          \
+      -DANDROID_SYSROOT=${PLATFORM}                                         \
+      -DANDROID_ABI="armeabi-v7a with NEON"                                 \
+      -DANDROID_TOOLCHAIN_NAME="arm-linux-androideabi-4.8"                  \
+      -DANDROID_NATIVE_API_LEVEL=android-17                                 \
+      -DANDROID_STL=none                                                    \
+      ../../
+
+# ----------------------------------------------------------------------------
+# usefull cmake debug flag
+# ----------------------------------------------------------------------------
+      #-DCMAKE_BUILD_TYPE=Debug                                              \
+      #-DCMAKE_VERBOSE_MAKEFILE=true                                         \
+      #--trace                                                               \
+      #--debug-output                                                        \
+
+#cmake --build . --clean-first -- V=1
+cmake --build .
+
+# ----------------------------------------------------------------------------
+# test script
+# ----------------------------------------------------------------------------
+#adb push osal/test/rk_log_test /system/bin/
+#adb push osal/test/rk_thread_test /system/bin/
+#adb shell sync
+#adb shell logcat -c
+#adb shell rk_log_test
+#adb shell rk_thread_test
+#adb logcat -d|tail -30
+
+
+
+
+
