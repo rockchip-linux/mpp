5ab2877839b1a83af39c3cdcb04b71a691989795
ChenHengming <ChenHengming@6e48237b-75ef-9749-8fc9-41990f28c85a>
2015-08-14 07:12:50 +0000

    [osal]: add mpp_common.h for macro
    git-svn-id: https://10.10.10.66:8443/svn/MediaProcessPlatform/trunk/mpp@108 6e48237b-75ef-9749-8fc9-41990f28c85a

---
+++ osal/inc/mpp_common.h
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2010 Rockchip Electronics S.LSI Co. LTD
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __MPP_COMMON_H__
+#define __MPP_COMMON_H__
+
+#include "rk_type.h"
+
+#define MPP_MAX(a, b)           ((a) > (b) ? (a) : (b))
+#define MPP_MAX3(a, b, c)       MPP_MAX(MPP_MAX(a,b),c)
+#define MPP_MAX4(a, b, c, d)    MPP_MAX((a), MPP_MAX3((b), (c), (d)))
+
+#define MPP_MIN(a,b)            ((a) > (b) ? (b) : (a))
+#define MPP_MIN3(a,b,c)         MPP_MIN(MPP_MIN(a,b),c)
+#define MPP_MIN4(a, b, c, d)    MPP_MIN((a), MPP_MIN3((b), (c), (d)))
+
+#define MPP_SWAP(type, a, b)    do {type SWAP_tmp = b; b = a; a = SWAP_tmp;} while(0)
+#define MPP_ARRAY_ELEMS(a)      (sizeof(a) / sizeof((a)[0]))
+#define MPP_ALIGN(x, a)         (((x)+(a)-1)&~((a)-1))
+#define MPP_VSWAP(a, b)         { a ^= b; b ^= a; a ^= b; }
+
+
+#if defined(_WIN32) && !defined(__MINGW32CE__)
+#define fseeko _fseeki64
+#elif defined(__MINGW32CE__)
+#define fseeko  fseeko64
+#endif
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+RK_S32 mpp_log2(RK_U32 v);
+RK_S32 mpp_log2_16bit(RK_U32 v);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /*__MPP_COMMON_H__*/
+
---
+++ osal/mpp_common.cpp
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2010 Rockchip Electronics S.LSI Co. LTD
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "mpp_common.h"
+
+static const RK_U8 log2_tab[256]={
+    0,0,1,1,2,2,2,2,3,3,3,3,3,3,3,3,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,
+    5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,
+    6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,
+    6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,
+    7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,
+    7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,
+    7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,
+    7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7
+};
+
+RK_S32 mpp_log2(RK_U32 v)
+{
+    RK_S32 n = 0;
+    if (v & 0xffff0000) {
+        v >>= 16;
+        n += 16;
+    }
+    if (v & 0xff00) {
+        v >>= 8;
+        n += 8;
+    }
+    n += log2_tab[v];
+
+    return n;
+}
+
+RK_S32 mpp_log2_16bit(RK_U32 v)
+{
+    RK_S32 n = 0;
+    if (v & 0xff00) {
+        v >>= 8;
+        n += 8;
+    }
+    n += log2_tab[v];
+
+    return n;
+}
+
