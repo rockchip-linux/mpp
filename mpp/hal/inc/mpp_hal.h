d03943bcf115a572c90b05b77f126318dfd46e15
ChenHengming <ChenHengming@6e48237b-75ef-9749-8fc9-41990f28c85a>
2015-08-26 12:47:15 +0000

    [mpp_hal]: add mpp_hal.h
    git-svn-id: https://10.10.10.66:8443/svn/MediaProcessPlatform/trunk/mpp@169 6e48237b-75ef-9749-8fc9-41990f28c85a

---
+++ mpp/hal/inc/mpp_hal.h
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2010 Rockchip Electronics S.LSI Co. LTD
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __HAL_TASK__
+#define __HAL_TASK__
+
+#include "rk_type.h"
+
+#define MAX_REF_SIZE    17
+
+/*
+ *  HalTask memory layout:
+ *
+ *  +----^----+ +----------------------+ +----^----+
+ *       |      |     context type     |      |
+ *       |      +----------------------+      |
+ *       +      |      coding type     |      |
+ *     header   +----------------------+      |
+ *       +      |         size         |      |
+ *       |      +----------------------+      |
+ *       |      |     pointer count    |      |
+ *  +----v----+ +----------------------+      |
+ *              |                      |      |
+ *              |       pointers       |      |
+ *              |                      |      +
+ *              +----------------------+    size
+ *              |                      |      +
+ *              |        data_0        |      |
+ *              |                      |      |
+ *              +----------------------+      |
+ *              |                      |      |
+ *              |        data_1        |      |
+ *              |                      |      |
+ *              +----------------------+      |
+ *              |                      |      |
+ *              |                      |      |
+ *              |        data_2        |      |
+ *              |                      |      |
+ *              |                      |      |
+ *              +----------------------+ +----v----+
+ */
+typedef struct {
+    MppCtxType      type;
+    MppCodingType   coding;
+    RK_U32          size;
+    RK_U32          count;
+
+    // current tesk output buffer
+    MppBuffer       dst;
+    // current task reference buffers
+    MppBuffer       refer[MAX_REF_SIZE];
+
+    void            *pointers[1];
+} MppHalDecTask;
+
+void HalTaskSetDstBuffer();
+void HalTaskSetDpb();
+void HalTaskSetDXVA();
+
+#endif /*__HAL_TASK__*/
+
