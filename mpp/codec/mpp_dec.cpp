b160c4e7c873410a02b26e80912bfab7e8aef413
ChenHengming <ChenHengming@6e48237b-75ef-9749-8fc9-41990f28c85a>
2015-10-20 23:52:14 +0000

    [mpp_dec]: enable set_default_width_height control
    git-svn-id: https://10.10.10.66:8443/svn/MediaProcessPlatform/trunk/mpp@408 6e48237b-75ef-9749-8fc9-41990f28c85a

--- mpp/codec/mpp_dec.cpp
+++ mpp/codec/mpp_dec.cpp
@@ -626,7 +626,7 @@ MPP_RET mpp_dec_control(MppDec *dec, MpiCmd cmd, void *param)
         mpp_frame_set_width(frame, p->ImgWidth);
         mpp_frame_set_height(frame, p->ImgHeight);
         mpp_buf_slot_init(&dec->frame_slots);
-        //mpp_slots_set_prop(dec->frame_slots, SLOTS_FRAME_INFO, frame);
+        mpp_slots_set_prop(dec->frame_slots, SLOTS_FRAME_INFO, frame);
         mpp_frame_deinit(&frame);
     } break;
     default : {
