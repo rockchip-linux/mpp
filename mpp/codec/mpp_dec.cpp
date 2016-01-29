45de45c61a927c2c5bb795c7f9bd8c4103da45be
ChenSiyong <ChenSiyong@6e48237b-75ef-9749-8fc9-41990f28c85a>
2016-01-29 08:43:34 +0000

    [mpp_dec]:add debug log for mpi thread destory
    git-svn-id: https://10.10.10.66:8443/svn/MediaProcessPlatform/trunk/mpp@559 6e48237b-75ef-9749-8fc9-41990f28c85a

--- mpp/codec/mpp_dec.cpp
+++ mpp/codec/mpp_dec.cpp
@@ -566,7 +566,7 @@ void *mpp_dec_parser_thread(void *data)
             continue;
 
     }
-
+    mpp_log("mpp_dec_parser_thread exit");
     if (NULL != task.hnd && task_dec->valid) {
         mpp_buf_slot_set_flag(packet_slots, task_dec->input, SLOT_CODEC_READY);
         mpp_buf_slot_set_flag(packet_slots, task_dec->input, SLOT_HAL_INPUT);
@@ -575,6 +575,7 @@ void *mpp_dec_parser_thread(void *data)
             mpp_buffer_put(task.hal_pkt_buf_in);
     }
     mpp_buffer_group_clear(mpp->mPacketGroup);
+    mpp_log("mpp_dec_parser_thread exit ok");
     return NULL;
 }
 
@@ -695,6 +696,7 @@ void *mpp_dec_hal_thread(void *data)
         }
     }
 
+    mpp_log("mpp_dec_hal_thread exit ok");
     return NULL;
 }
 
