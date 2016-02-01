335967caccd6196eea7c9b162dd1982a4a5a52c2
ChenSiyong <ChenSiyong@6e48237b-75ef-9749-8fc9-41990f28c85a>
2016-02-01 09:51:12 +0000

    [vp9_d]fix compile error
    git-svn-id: https://10.10.10.66:8443/svn/MediaProcessPlatform/trunk/mpp@564 6e48237b-75ef-9749-8fc9-41990f28c85a

--- osal/inc/mpp_common.h
+++ osal/inc/mpp_common.h
@@ -34,6 +34,89 @@
 #define MPP_ALIGN(x, a)         (((x)+(a)-1)&~((a)-1))
 #define MPP_VSWAP(a, b)         { a ^= b; b ^= a; a ^= b; }
 
+#define MPP_RB16(x)  ((((const uint8_t*)(x))[0] << 8) | ((const uint8_t*)(x))[1])
+#define MPP_WB16(p, d) do { \
+        ((uint8_t*)(p))[1] = (d); \
+        ((uint8_t*)(p))[0] = (d)>>8; } while(0)
+
+#define MPP_RL16(x)  ((((const uint8_t*)(x))[1] << 8) | \
+                     ((const uint8_t*)(x))[0])
+#define MPP_WL16(p, d) do { \
+        ((uint8_t*)(p))[0] = (d); \
+        ((uint8_t*)(p))[1] = (d)>>8; } while(0)
+
+#define MPP_RB32(x)  ((((const uint8_t*)(x))[0] << 24) | \
+                     (((const uint8_t*)(x))[1] << 16) | \
+                     (((const uint8_t*)(x))[2] <<  8) | \
+                     ((const uint8_t*)(x))[3])
+#define MPP_WB32(p, d) do { \
+        ((uint8_t*)(p))[3] = (d); \
+        ((uint8_t*)(p))[2] = (d)>>8; \
+        ((uint8_t*)(p))[1] = (d)>>16; \
+        ((uint8_t*)(p))[0] = (d)>>24; } while(0)
+
+#define MPP_RL32(x) ((((const uint8_t*)(x))[3] << 24) | \
+                    (((const uint8_t*)(x))[2] << 16) | \
+                    (((const uint8_t*)(x))[1] <<  8) | \
+                    ((const uint8_t*)(x))[0])
+#define MPP_WL32(p, d) do { \
+        ((uint8_t*)(p))[0] = (d); \
+        ((uint8_t*)(p))[1] = (d)>>8; \
+        ((uint8_t*)(p))[2] = (d)>>16; \
+        ((uint8_t*)(p))[3] = (d)>>24; } while(0)
+
+#define MPP_RB64(x)  (((uint64_t)((const uint8_t*)(x))[0] << 56) | \
+                     ((uint64_t)((const uint8_t*)(x))[1] << 48) | \
+                     ((uint64_t)((const uint8_t*)(x))[2] << 40) | \
+                     ((uint64_t)((const uint8_t*)(x))[3] << 32) | \
+                     ((uint64_t)((const uint8_t*)(x))[4] << 24) | \
+                     ((uint64_t)((const uint8_t*)(x))[5] << 16) | \
+                     ((uint64_t)((const uint8_t*)(x))[6] <<  8) | \
+                     (uint64_t)((const uint8_t*)(x))[7])
+#define MPP_WB64(p, d) do { \
+        ((uint8_t*)(p))[7] = (d);     \
+        ((uint8_t*)(p))[6] = (d)>>8;  \
+        ((uint8_t*)(p))[5] = (d)>>16; \
+        ((uint8_t*)(p))[4] = (d)>>24; \
+        ((uint8_t*)(p))[3] = (d)>>32; \
+        ((uint8_t*)(p))[2] = (d)>>40; \
+        ((uint8_t*)(p))[1] = (d)>>48; \
+        ((uint8_t*)(p))[0] = (d)>>56; } while(0)
+
+#define MPP_RL64(x)  (((uint64_t)((const uint8_t*)(x))[7] << 56) | \
+                     ((uint64_t)((const uint8_t*)(x))[6] << 48) | \
+                     ((uint64_t)((const uint8_t*)(x))[5] << 40) | \
+                     ((uint64_t)((const uint8_t*)(x))[4] << 32) | \
+                     ((uint64_t)((const uint8_t*)(x))[3] << 24) | \
+                     ((uint64_t)((const uint8_t*)(x))[2] << 16) | \
+                     ((uint64_t)((const uint8_t*)(x))[1] <<  8) | \
+                     (uint64_t)((const uint8_t*)(x))[0])
+#define MPP_WL64(p, d) do { \
+        ((uint8_t*)(p))[0] = (d);     \
+        ((uint8_t*)(p))[1] = (d)>>8;  \
+        ((uint8_t*)(p))[2] = (d)>>16; \
+        ((uint8_t*)(p))[3] = (d)>>24; \
+        ((uint8_t*)(p))[4] = (d)>>32; \
+        ((uint8_t*)(p))[5] = (d)>>40; \
+        ((uint8_t*)(p))[6] = (d)>>48; \
+        ((uint8_t*)(p))[7] = (d)>>56; } while(0)
+
+#define MPP_RB24(x)  ((((const uint8_t*)(x))[0] << 16) | \
+                     (((const uint8_t*)(x))[1] <<  8) | \
+                     ((const uint8_t*)(x))[2])
+#define MPP_WB24(p, d) do { \
+        ((uint8_t*)(p))[2] = (d); \
+        ((uint8_t*)(p))[1] = (d)>>8; \
+        ((uint8_t*)(p))[0] = (d)>>16; } while(0)
+
+#define MPP_RL24(x)  ((((const uint8_t*)(x))[2] << 16) | \
+                     (((const uint8_t*)(x))[1] <<  8) | \
+                     ((const uint8_t*)(x))[0])
+
+#define MPP_WL24(p, d) do { \
+        ((uint8_t*)(p))[0] = (d); \
+        ((uint8_t*)(p))[1] = (d)>>8; \
+        ((uint8_t*)(p))[2] = (d)>>16; } while(0)
 
 #include <stdio.h>
 #if defined(_WIN32) && !defined(__MINGW32CE__)
