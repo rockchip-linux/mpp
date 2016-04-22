a0723a356e72fe7e12e83a2fe9ca95a3dfcd00c7
ChenHengming <ChenHengming@6e48237b-75ef-9749-8fc9-41990f28c85a>
2016-04-22 02:56:03 +0000

    [Android]: Add 64bit script
    NOTE: there will be ndk error:
    
    ../libosal.a(os_env.c.o): In function `os_get_env_u32':
    os_env.c:(.text.os_get_env_u32+0x20): undefined reference to `__system_property_get'
    ../libosal.a(os_env.c.o): In function `os_get_env_str':
    os_env.c:(.text.os_get_env_str+0x34): undefined reference to `__system_property_get'
    ../libosal.a(os_env.c.o): In function `os_set_env_u32':
    os_env.c:(.text.os_set_env_u32+0x34): undefined reference to `__system_property_set'
    ../libosal.a(os_env.c.o): In function `os_set_env_str':
    os_env.c:(.text.os_set_env_str+0x8): undefined reference to `__system_property_set'
    
    Solution here: http://www.bubuko.com/infodetail-1014879.html
    
    
    git-svn-id: https://10.10.10.66:8443/svn/MediaProcessPlatform/trunk/mpp@613 6e48237b-75ef-9749-8fc9-41990f28c85a

---
+++ build/android/make-Android-64bit.bash
@@ -0,0 +1,45 @@
+#!/bin/bash
+# Run this from within a bash shell
+HOST_IP=`hostname --all-ip-addresses`
+if [ ${HOST_IP} == "10.10.10.65" ] || [ ${HOST_IP} == "10.10.10.67" ]; then
+    ANDROID_NDK=/home/pub/ndk/android-ndk-r10d/
+else
+    ANDROID_NDK=~/work/android/ndk/android-ndk-r10d/
+fi
+
+PLATFORM=$ANDROID_NDK/platforms/android-21/arch-arm64
+
+cmake -DCMAKE_TOOLCHAIN_FILE=./android.toolchain.cmake                      \
+      -DCMAKE_BUILD_TYPE=Release                                            \
+      -DANDROID_FORCE_ARM_BUILD=ON                                          \
+      -DANDROID_NDK=${ANDROID_NDK}                                          \
+      -DANDROID_SYSROOT=${PLATFORM}                                         \
+      -DANDROID_ABI="arm64-v8a"                                             \
+      -DANDROID_TOOLCHAIN_NAME="aarch64-linux-android-4.9"                  \
+      -DANDROID_NATIVE_API_LEVEL=android-21                                 \
+      -DANDROID_STL=system                                                  \
+      ../../
+
+# ----------------------------------------------------------------------------
+# usefull cmake debug flag
+# ----------------------------------------------------------------------------
+      #-DCMAKE_BUILD_TYPE=Debug                                              \
+      #-DCMAKE_VERBOSE_MAKEFILE=true                                         \
+      #--trace                                                               \
+      #--debug-output                                                        \
+
+#cmake --build . --clean-first -- V=1
+cmake --build .
+
+# ----------------------------------------------------------------------------
+# test script
+# ----------------------------------------------------------------------------
+#adb push osal/test/rk_log_test /system/bin/
+#adb push osal/test/rk_thread_test /system/bin/
+#adb shell sync
+#adb shell logcat -c
+#adb shell rk_log_test
+#adb shell rk_thread_test
+#adb logcat -d|tail -30
+
+
