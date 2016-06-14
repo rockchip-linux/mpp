4a632c9b70f1af4e6abe81c7ade76f7227c0a4a4
ChenHengming <ChenHengming@6e48237b-75ef-9749-8fc9-41990f28c85a>
2016-06-14 08:34:38 +0000

    [mpp_packet]: add check function for MppPacket
    git-svn-id: https://10.10.10.66:8443/svn/MediaProcessPlatform/trunk/mpp@910 6e48237b-75ef-9749-8fc9-41990f28c85a

--- mpp/base/mpp_packet.cpp
+++ mpp/base/mpp_packet.cpp
@@ -28,12 +28,15 @@ static const char *module_name = MODULE_TAG;
 #define setup_mpp_packet_name(packet) \
     ((MppPacketImpl*)packet)->name = module_name;
 
-#define check_mpp_packet_name(packet) \
-    do { \
-        mpp_assert(((MppPacketImpl*)packet)->name == module_name); \
-        if (((MppPacketImpl*)packet)->name != module_name) \
-            mpp_err_f("packet %p failed on name checking\n"); \
-    } while (0)
+MPP_RET check_is_mpp_packet(void *packet)
+{
+    if (packet && ((MppPacketImpl*)packet)->name == module_name)
+        return MPP_OK;
+
+    mpp_err_f("pointer %p failed on check\n");
+    mpp_abort();
+    return MPP_NOK;
+}
 
 MPP_RET mpp_packet_new(MppPacket *packet)
 {
@@ -94,15 +97,14 @@ MPP_RET mpp_packet_init_with_buffer(MppPacket *packet, MppBuffer buffer)
 
 MPP_RET mpp_packet_copy_init(MppPacket *packet, const MppPacket src)
 {
-    if (NULL == packet || NULL == src) {
-        mpp_err_f("found NULL input\n");
-        return MPP_ERR_NULL_PTR;
+    if (NULL == packet || check_is_mpp_packet(src)) {
+        mpp_err_f("found invalid input %p %p\n", packet, src);
+        return MPP_ERR_UNKNOW;
     }
 
-    MppPacketImpl *src_impl = (MppPacketImpl *)src;
-
-    check_mpp_packet_name(src_impl);
     *packet = NULL;
+
+    MppPacketImpl *src_impl = (MppPacketImpl *)src;
     MppPacket pkt;
     MPP_RET ret = mpp_packet_new(&pkt);
     if (ret)
@@ -137,13 +139,12 @@ MPP_RET mpp_packet_copy_init(MppPacket *packet, const MppPacket src)
 
 MPP_RET mpp_packet_deinit(MppPacket *packet)
 {
-    if (NULL == packet || NULL == *packet) {
+    if (NULL == packet || check_is_mpp_packet(*packet)) {
         mpp_err_f("found NULL input\n");
         return MPP_ERR_NULL_PTR;
     }
 
     MppPacketImpl *p = (MppPacketImpl *)(*packet);
-    check_mpp_packet_name(p);
 
     /* release buffer reference */
     if (p->buffer)
@@ -160,8 +161,10 @@ MPP_RET mpp_packet_deinit(MppPacket *packet)
 
 void mpp_packet_set_pos(MppPacket packet, void *pos)
 {
+    if (check_is_mpp_packet(packet))
+        return ;
+
     MppPacketImpl *p = (MppPacketImpl *)packet;
-    check_mpp_packet_name(p);
     p->pos    = pos;
     p->length = p->size - ((char *)pos - (char *)p->data);
     mpp_assert(p->data <= p->pos);
@@ -170,57 +173,47 @@ void mpp_packet_set_pos(MppPacket packet, void *pos)
 
 void *mpp_packet_get_pos(const MppPacket packet)
 {
+    if (check_is_mpp_packet(packet))
+        return NULL;
+
     MppPacketImpl *p = (MppPacketImpl *)packet;
-    check_mpp_packet_name(p);
     return p->pos;
 }
 
 MPP_RET mpp_packet_set_eos(MppPacket packet)
 {
-    if (NULL == packet) {
-        mpp_err_f("found NULL input\n");
-        return MPP_ERR_NULL_PTR;
-    }
+    if (check_is_mpp_packet(packet))
+        return MPP_ERR_UNKNOW;
 
     MppPacketImpl *p = (MppPacketImpl *)packet;
-    check_mpp_packet_name(p);
     p->flag |= MPP_PACKET_FLAG_EOS;
     return MPP_OK;
 }
 
 RK_U32 mpp_packet_get_eos(MppPacket packet)
 {
-    if (NULL == packet) {
-        mpp_err_f("found NULL input\n");
-        return MPP_ERR_NULL_PTR;
-    }
+    if (check_is_mpp_packet(packet))
+        return 0;
 
     MppPacketImpl *p = (MppPacketImpl *)packet;
-    check_mpp_packet_name(p);
     return (p->flag & MPP_PACKET_FLAG_EOS) ? (1) : (0);
 }
 
 MPP_RET mpp_packet_set_extra_data(MppPacket packet)
 {
-    if (NULL == packet) {
-        mpp_err_f("found NULL input\n");
-        return MPP_ERR_NULL_PTR;
-    }
+    if (check_is_mpp_packet(packet))
+        return MPP_ERR_UNKNOW;
 
     MppPacketImpl *p = (MppPacketImpl *)packet;
-    check_mpp_packet_name(p);
     p->flag |= MPP_PACKET_FLAG_EXTRA_DATA;
     return MPP_OK;
 }
 
 MPP_RET mpp_packet_reset(MppPacketImpl *packet)
 {
-    if (NULL == packet) {
-        mpp_err_f("found NULL input\n");
-        return MPP_ERR_NULL_PTR;
-    }
+    if (check_is_mpp_packet(packet))
+        return MPP_ERR_UNKNOW;
 
-    check_mpp_packet_name(packet);
     memset(packet, 0, sizeof(*packet));
     setup_mpp_packet_name(packet);
     return MPP_OK;
@@ -228,13 +221,10 @@ MPP_RET mpp_packet_reset(MppPacketImpl *packet)
 
 void mpp_packet_set_buffer(MppPacket packet, MppBuffer buffer)
 {
-    if (NULL == packet) {
-        mpp_err_f("found NULL input\n");
+    if (check_is_mpp_packet(packet))
         return ;
-    }
 
     MppPacketImpl *p = (MppPacketImpl *)packet;
-    check_mpp_packet_name(p);
     if (p->buffer) {
         mpp_buffer_put(p->buffer);
     }
@@ -246,19 +236,16 @@ void mpp_packet_set_buffer(MppPacket packet, MppBuffer buffer)
 
 MppBuffer mpp_packet_get_buffer(const MppPacket packet)
 {
-    if (NULL == packet) {
-        mpp_err_f("found NULL input\n");
+    if (check_is_mpp_packet(packet))
         return NULL;
-    }
 
     MppPacketImpl *p = (MppPacketImpl *)packet;
-    check_mpp_packet_name(p);
     return p->buffer;
 }
 
 MPP_RET mpp_packet_read(MppPacket packet, size_t offset, void *data, size_t size)
 {
-    if (NULL == packet || NULL == data) {
+    if (check_is_mpp_packet(packet) || NULL == data) {
         mpp_err_f("invalid input: packet %p data %p\n", packet, data);
         return MPP_ERR_UNKNOW;
     }
@@ -274,7 +261,7 @@ MPP_RET mpp_packet_read(MppPacket packet, size_t offset, void *data, size_t size
 
 MPP_RET mpp_packet_write(MppPacket packet, size_t offset, void *data, size_t size)
 {
-    if (NULL == packet || NULL == data) {
+    if (check_is_mpp_packet(packet) || NULL == data) {
         mpp_err_f("invalid input: packet %p data %p\n", packet, data);
         return MPP_ERR_UNKNOW;
     }
@@ -294,12 +281,12 @@ MPP_RET mpp_packet_write(MppPacket packet, size_t offset, void *data, size_t siz
 #define MPP_PACKET_ACCESSORS(type, field) \
     type mpp_packet_get_##field(const MppPacket s) \
     { \
-        check_mpp_packet_name(s); \
+        check_is_mpp_packet(s); \
         return ((MppPacketImpl*)s)->field; \
     } \
     void mpp_packet_set_##field(MppPacket s, type v) \
     { \
-        check_mpp_packet_name(s); \
+        check_is_mpp_packet(s); \
         ((MppPacketImpl*)s)->field = v; \
     }
 
