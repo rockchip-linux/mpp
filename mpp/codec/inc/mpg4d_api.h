d5afc2102b8461aeeb0910f2068f402024e3fd0d
ChenSiyong <ChenSiyong@6e48237b-75ef-9749-8fc9-41990f28c85a>
2016-06-21 10:00:08 +0000

    [h265d] revert 882 modify for hevc modify [mpg4_d] fix no mpg4d_api.h cause compile error
    git-svn-id: https://10.10.10.66:8443/svn/MediaProcessPlatform/trunk/mpp@944 6e48237b-75ef-9749-8fc9-41990f28c85a

---
+++ mpp/codec/inc/mpg4d_api.h
@@ -0,0 +1,17 @@
+#ifndef __MPG4D_API_H__
+#define __MPG4D_API_H__
+
+#include "parser_api.h"
+
+#ifdef  __cplusplus
+extern "C" {
+#endif
+
+extern const ParserApi api_mpg4d_parser;
+
+#ifdef  __cplusplus
+}
+#endif
+
+#endif
+
