58cf49779a5f43303bfa69a980e4a400898a94f0
ChenHengming <ChenHengming@6e48237b-75ef-9749-8fc9-41990f28c85a>
2016-07-19 03:44:11 +0000

    [mpp_buf_slot]: set default buffer size rate to width * height * 9 / 5
    git-svn-id: https://10.10.10.66:8443/svn/MediaProcessPlatform/trunk/mpp@1042 6e48237b-75ef-9749-8fc9-41990f28c85a

--- build/android/update.sh
+++ build/android/update.sh
@@ -2,9 +2,14 @@
 adb remount
 adb push ./mpp/libmpp.so /system/lib/
 adb push ./mpp/legacy/libvpu.so /system/lib/
+adb push ./test/mpi_dec_test /system/bin/
 #adb push librk_vpuapi.so /system/lib/
 
 adb shell sync
 
 adb shell busybox pkill mediaserver
 
+adb shell logcat -c
+#adb shell mpi_dec_test -i /sdcard/beauty.mpg -t 4
+adb shell logcat -v time > log.txt
+#adb shell logcat | grep vpu > log.txt
