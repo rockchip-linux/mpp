d2dceaf8eb962c1c011d592e301e84ddea5f52ac
GaoZhili <GaoZhili@6e48237b-75ef-9749-8fc9-41990f28c85a>
2016-07-22 12:37:30 +0000

    [h264e_macro]: add the log interface in h264e_macro.h
    git-svn-id: https://10.10.10.66:8443/svn/MediaProcessPlatform/trunk/mpp@1081 6e48237b-75ef-9749-8fc9-41990f28c85a

--- mpp/codec/enc/h264/src/h264e_macro.h
+++ mpp/codec/enc/h264/src/h264e_macro.h
@@ -19,27 +19,44 @@
 
 //#define SYNTAX_DATA_IN_FILE 1
 
-#define H264E_CONTROL_LOG_MODE              0x00001111
+#define H264E_CONTROL_LOG_MODE              0x00000111
 
 #define H264E_CONTROL_LOG_ERROR             0x00000001
 #define H264E_CONTROL_LOG_ASSERT            0x00000010
 #define H264E_CONTROL_LOG_WARNNING          0x00000100
-#define H264E_CONTROL_LOG_FLOW              0x00001000
+#define H264E_CONTROL_LOG_DEBUG             0x00001000
 #define H264E_CONTROL_LOG_DETAIL            0x00010000
 #define H264E_CONTROL_LOG_FILE              0x00100000
 
 
 #define h264e_control_debug_enter() \
     do {\
-        if (H264E_CONTROL_LOG_MODE & H264E_CONTROL_LOG_FLOW)\
+        if (H264E_CONTROL_LOG_MODE & H264E_CONTROL_LOG_DEBUG)\
             { mpp_log("line(%d), func(%s), enter", __LINE__, __FUNCTION__); }\
     } while (0)
 
 #define h264e_control_debug_leave() \
     do {\
-        if (H264E_CONTROL_LOG_MODE & H264E_CONTROL_LOG_FLOW)\
+        if (H264E_CONTROL_LOG_MODE & H264E_CONTROL_LOG_DEBUG)\
             { mpp_log("line(%d), func(%s), leave", __LINE__, __FUNCTION__); }\
     } while (0)
 
+#define h264e_control_err(fmt, ...)\
+    do {\
+        if (H264E_CONTROL_LOG_MODE & H264E_CONTROL_LOG_ERROR)\
+            { mpp_log(fmt, ## __VA_ARGS__); }\
+    } while (0)
+
+#define h264e_control_warning(fmt, ...)\
+    do {\
+        if (H264E_CONTROL_LOG_MODE & H264E_CONTROL_LOG_WARNNING)\
+            { mpp_log(fmt, ## __VA_ARGS__); }\
+    } while (0)
+
+#define h264e_control_log(fmt, ...)\
+    do {\
+        if (H264E_CONTROL_LOG_MODE & H264E_CONTROL_LOG_DEBUG)\
+            {  mpp_log(fmt, ## __VA_ARGS__); }\
+    } while (0)
 
 #endif
