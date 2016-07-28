41f3fb8cc80bf89f0062ceb1eabf162c8c48bbba
ZhaoJun <ZhaoJun@6e48237b-75ef-9749-8fc9-41990f28c85a>
2016-07-28 03:43:15 +0000

    [sofia-3gr-linux]: add sofia-3gr makefile
    git-svn-id: https://10.10.10.66:8443/svn/MediaProcessPlatform/trunk/mpp@1114 6e48237b-75ef-9749-8fc9-41990f28c85a

--- build/linux/make-arm-Makefiles.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Run this from within a bash shell
-cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=debug \
-    -DCMAKE_TOOLCHAIN_FILE=./arm.linux.cross.cmake \
-    -DCMAKE_RKPLATFORM_ENABLE=ON                   \
-    ../../  && ccmake  ../../
---
+++ build/linux/x86/sofia-3gr/sofia-3gr.linux.cross.cmake
@@ -0,0 +1,6 @@
+cmake_minimum_required( VERSION 2.6.3 )
+
+SET(CMAKE_SYSTEM_NAME Linux)
+add_definitions(-fPIC)
+add_definitions(-DSOFIA_3GR_LINUX)
+include_directories(osal)
