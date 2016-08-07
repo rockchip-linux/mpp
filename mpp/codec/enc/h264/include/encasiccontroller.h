20717fb0e9889d0ebffb4cd091335a57bad28614
ChenHengming <ChenHengming@6e48237b-75ef-9749-8fc9-41990f28c85a>
2016-08-07 05:39:41 +0000

    [h264e]: remove redundant code
    git-svn-id: https://10.10.10.66:8443/svn/MediaProcessPlatform/trunk/mpp@1166 6e48237b-75ef-9749-8fc9-41990f28c85a

--- mpp/codec/enc/h264/include/encasiccontroller.h
+++ mpp/codec/enc/h264/include/encasiccontroller.h
@@ -22,7 +22,7 @@
 #include "ewl.h"
 #include "mpp_mem.h"
 #include "mpp_buffer.h"
-#include "h264e_syntax.h"  // add by lance 2016.05.07
+#include "h264e_syntax.h"
 
 /* HW status register bits */
 #define ASIC_STATUS_BUFF_FULL           0x020
@@ -75,21 +75,10 @@ typedef enum {
 } asicFrameCodingType_e;
 
 typedef struct {
-    u32 irqDisable;
-    u32 irqInterval;
-    u32 mbsInCol;
-    u32 mbsInRow;
     u32 qp;
     u32 qpMin;
     u32 qpMax;
-    u32 constrainedIntraPrediction;
-    u32 roundingCtrl;
     u32 frameCodingType;
-    u32 codingType;
-    u32 pixelsOnRow;
-    u32 xFill;
-    u32 yFill;
-    u32 ppsId;
     u32 idrPicId;
     u32 frameNum;
     u32 picInitQp;
@@ -105,12 +94,6 @@ typedef struct {
     u32 inputImageRotation;
     u32 outputStrmBase;
     u32 outputStrmSize;
-    u32 firstFreeBit;
-    u32 strmStartMSB;
-    u32 strmStartLSB;
-    u32 rlcBase;
-    u32 rlcLimitSpace;
-    u32 socket;   // vpu socket    // now it will be inited by hal part, so mask it    by lance 2016.05.09
 
     u32 inputLumBase;
     u32 inputCbBase;
@@ -123,31 +106,12 @@ typedef struct {
     u32 rlcCount;
     u32 qpSum;
     u32 h264StrmMode;   /* 0 - byte stream, 1 - NAL units */
-    u32 gobHeaderMask;
-    u32 gobFrameId;
-    u8 quantTable[8 * 8 * 2];
-    u32 jpegMode;
-    u32 jpegSliceEnable;
-    u32 jpegRestartInterval;
-    u32 jpegRestartMarker;
     u32 inputLumaBaseOffset;
     u32 inputChromaBaseOffset;
     u32 h264Inter4x4Disabled;
     u32 disableQuarterPixelMv;
     u32 vsNextLumaBase;
     u32 vsMode;
-    u32 vpSize;
-    u32 vpMbBits;
-    u32 hec;
-    u32 moduloTimeBase;
-    u32 intraDcVlcThr;
-    u32 vopFcode;
-    u32 timeInc;
-    u32 timeIncBits;
-    u32 asicCfgReg;
-    u32 intra16Favor;
-    u32 interFavor;
-    u32 skipPenalty;
     i32 madQpDelta;
     u32 madThreshold;
     u32 madCount;
@@ -159,24 +123,6 @@ typedef struct {
     u32 rMaskMsb;
     u32 gMaskMsb;
     u32 bMaskMsb;
-
-    u32 splitMvMode;
-    u32 diffMvPenalty[3];
-    u32 splitPenalty[4];
-    u32 zeroMvFavorDiv2;
-
-    u32 intraAreaTop;
-    u32 intraAreaLeft;
-    u32 intraAreaBottom;
-    u32 intraAreaRight;
-    u32 roi1Top;
-    u32 roi1Left;
-    u32 roi1Bottom;
-    u32 roi1Right;
-    u32 roi2Top;
-    u32 roi2Left;
-    u32 roi2Bottom;
-    u32 roi2Right;
     u32 hw_status;
 } regValues_s;
 
@@ -190,5 +136,5 @@ typedef struct {
 i32 EncAsicControllerInit(asicData_s * asic);
 
 /* Functions for controlling ASIC */
-void EncAsicFrameStart(void * inst,/* const void *ewl,*/ regValues_s * val, h264e_syntax *syntax_data);  // mask by lance 2016.05.12
+void EncAsicFrameStart(void * inst, regValues_s * val, h264e_syntax *syntax_data);
 #endif
