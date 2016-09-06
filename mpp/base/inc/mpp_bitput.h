e3c564b29e9b5432489f0dbecb016abba88de8bd
Herman Chen <herman.chen@rock-chips.com>
2016-09-06 16:29:29 +0800

    [format]: fix mpp_bitput format
    Change-Id: I8cb8c3c38319be5bb68e9a50006f80b904703981
    Signed-off-by: Herman Chen <herman.chen@rock-chips.com>

--- mpp/base/inc/mpp_bitput.h
+++ mpp/base/inc/mpp_bitput.h
@@ -1,28 +1,30 @@
 /*
-*
-* Copyright 2015 Rockchip Electronics Co. LTD
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*      http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ *
+ * Copyright 2015 Rockchip Electronics Co. LTD
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #ifndef __MPP_BITPUT_H__
 #define __MPP_BITPUT_H__
+
 #include <stdio.h>
 #include <assert.h>
+
 #include "rk_type.h"
 #include "mpp_log.h"
 #include "mpp_common.h"
 #include "mpp_err.h"
-
 
 typedef struct bitput_ctx_t {
     RK_U32          buflen;         //!< max buf length, 64bit uint
@@ -36,10 +38,13 @@ typedef struct bitput_ctx_t {
 #ifdef  __cplusplus
 extern "C" {
 #endif
+
 RK_S32 mpp_set_bitput_ctx(BitputCtx_t *bp, RK_U64 *data, RK_U32 len);
 void mpp_put_bits(BitputCtx_t *bp, RK_U64 invalue, RK_S32 lbits);
 void mpp_put_align(BitputCtx_t *bp, RK_S32 align_bits, int flag);
+
 #ifdef  __cplusplus
 }
 #endif
+
 #endif
--- mpp/base/mpp_bitput.c
+++ mpp/base/mpp_bitput.c
@@ -1,21 +1,23 @@
 /*
-*
-* Copyright 2015 Rockchip Electronics Co. LTD
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*      http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ *
+ * Copyright 2015 Rockchip Electronics Co. LTD
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include <string.h>
 #include "mpp_bitput.h"
+
 RK_S32 mpp_set_bitput_ctx(BitputCtx_t *bp, RK_U64 *data, RK_U32 len)
 {
     memset(bp, 0, sizeof(BitputCtx_t));
