3ac58d94478fb9865b0fb1e4debc8a12a281c4ea
Herman Chen <herman.chen@rock-chips.com>
2016-09-09 14:25:28 +0800

    [cmake]: set linux/arm cmake script to Release
    Change-Id: Id99284a22713276c42b6eedaca3b4cd8695686e2
    Signed-off-by: Herman Chen <herman.chen@rock-chips.com>

--- build/linux/arm/make-Makefiles.bash
+++ build/linux/arm/make-Makefiles.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Run this from within a bash shell
-cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=debug \
-    -DCMAKE_TOOLCHAIN_FILE=./arm.linux.cross.cmake \
-    -DCMAKE_RKPLATFORM_ENABLE=ON                   \
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_TOOLCHAIN_FILE=./arm.linux.cross.cmake   \
+    -DCMAKE_RKPLATFORM_ENABLE=ON                     \
     ../../../  && cmake --build .
