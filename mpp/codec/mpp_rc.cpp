f73ee250c77951d9ee1d9c6b71285b6fca4b3ccd
Jung Zhao <jung.zhao@rock-chips.com>
2017-01-16 16:27:11 +0800

    [rc]: add init qp estimation
    if outside doesn't set init qp value, mpp will estimate one init qp
    according to resolution and bitrate.
    
    Change-Id: If5415f6a3bfa3f2981ffd4eb044614bf9b035d2f
    Signed-off-by: Jung Zhao <jung.zhao@rock-chips.com>

--- mpp/codec/mpp_rc.cpp
+++ mpp/codec/mpp_rc.cpp
@@ -37,58 +37,6 @@
 
 static RK_U32 mpp_rc_debug = 0;
 
-static RK_S32 axb_div_c(RK_S32 a, RK_S32 b, RK_S32 c)
-{
-    RK_U32 left = 32;
-    RK_U32 right = 0;
-    RK_U32 shift;
-    RK_S32 sign = 1;
-    RK_S32 tmp;
-
-    if (a == 0 || b == 0)
-        return 0;
-    else if ((a * b / b) == a && c != 0)
-        return (a * b / c);
-
-    if (a < 0) {
-        sign = -1;
-        a = -a;
-    }
-    if (b < 0) {
-        sign *= -1;
-        b = -b;
-    }
-    if (c < 0) {
-        sign *= -1;
-        c = -c;
-    }
-
-    if (c == 0)
-        return 0x7FFFFFFF * sign;
-
-    if (b > a) {
-        tmp = b;
-        b = a;
-        a = tmp;
-    }
-
-    for (--left; (((RK_U32)a << left) >> left) != (RK_U32)a; --left)
-        ;
-
-    left--;
-
-    while (((RK_U32)b >> right) > (RK_U32)c)
-        right++;
-
-    if (right > left) {
-        return 0x7FFFFFFF * sign;
-    } else {
-        shift = left - right;
-        return (RK_S32)((((RK_U32)a << shift) /
-                         (RK_U32)c * (RK_U32)b) >> shift) * sign;
-    }
-}
-
 MPP_RET mpp_data_init(MppData **data, RK_S32 size)
 {
     if (NULL == data || size <= 0) {
