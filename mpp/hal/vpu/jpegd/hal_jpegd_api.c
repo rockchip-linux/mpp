ac078a0a6110800e086d694b35fbaee83afb7faa
Randy Li <randy.li@rock-chips.com>
2017-03-07 16:28:24 +0800

    [jpegd] rename to its device type
    It is a implementation for the VDPU2 video IP.
    
    Change-Id: Id2d83b971cf7e75c592ca5d8dd0b65d3c4e0122e
    Signed-off-by: Randy Li <randy.li@rock-chips.com>

---
+++ mpp/hal/vpu/jpegd/hal_jpegd_api.c
@@ -0,0 +1,119 @@
+/*
+*
+* Copyright 2017 Rockchip Electronics Co. LTD
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+#define MODULE_TAG "hal_jpegd_api"
+
+#include <string.h>
+
+#include "rk_type.h"
+#include "mpp_hal.h"
+#include "mpp_platform.h"
+#include "hal_jpegd_base.h"
+#include "hal_jpegd_vdpu2.h"
+
+static MPP_RET hal_jpegd_reg_gen (void *hal, HalTaskInfo *task)
+{
+    JpegHalContext *self = (JpegHalContext *)hal;
+    return self->hal_api.reg_gen (hal, task);
+}
+
+static MPP_RET hal_jpegd_start (void *hal, HalTaskInfo *task)
+{
+    JpegHalContext *self = (JpegHalContext *)hal;
+    return self->hal_api.start (hal, task);
+}
+
+static MPP_RET hal_jpegd_wait (void *hal, HalTaskInfo *task)
+{
+    JpegHalContext *self = (JpegHalContext *)hal;
+    return self->hal_api.wait (hal, task);
+}
+
+static MPP_RET hal_jpegd_reset (void *hal)
+{
+    JpegHalContext *self = (JpegHalContext *)hal;
+    return self->hal_api.reset (hal);
+}
+
+static MPP_RET hal_jpegd_flush (void *hal)
+{
+    JpegHalContext *self = (JpegHalContext *)hal;
+    return self->hal_api.flush (hal);
+}
+
+static MPP_RET hal_jpegd_control (void *hal, RK_S32 cmd_type, void *param)
+{
+    JpegHalContext *self = (JpegHalContext *)hal;
+    return self->hal_api.control (hal, cmd_type, param);
+}
+
+static MPP_RET hal_jpegd_deinit (void *hal)
+{
+    JpegHalContext *self = (JpegHalContext *)hal;
+    return self->hal_api.deinit (hal);
+}
+
+static MPP_RET hal_jpegd_init (void *hal, MppHalCfg *cfg)
+{
+    JpegHalContext *self = (JpegHalContext *)hal;
+    MppHalApi *p_api = NULL;
+    VpuHardMode hard_mode = MODE_NULL;
+    RK_U32 hw_flag = 0;
+
+    if (NULL == self)
+        return MPP_ERR_VALUE;
+    memset(self, 0, sizeof(JpegHalContext));
+
+    p_api = &self->hal_api;
+
+    hw_flag = mpp_get_vcodec_type();
+    if (hw_flag & HAVE_VPU2)
+        hard_mode = VDPU2_MODE;
+
+    switch (hard_mode) {
+    case VDPU2_MODE:
+        p_api->init = hal_jpegd_vdpu2_init;
+        p_api->deinit = hal_jpegd_vdpu2_deinit;
+        p_api->reg_gen = hal_jpegd_vdpu2_gen_regs;
+        p_api->start = hal_jpegd_vdpu2_start;
+        p_api->wait = hal_jpegd_vdpu2_wait;
+        p_api->reset = hal_jpegd_vdpu2_reset;
+        p_api->flush = hal_jpegd_vdpu2_flush;
+        p_api->control = hal_jpegd_vdpu2_control;
+        break;
+    default:
+        return MPP_ERR_INIT;
+        break;
+    }
+
+    return p_api->init (hal, cfg);
+}
+
+const MppHalApi hal_api_jpegd = {
+    .name = "jpegd_rkdec",
+    .type = MPP_CTX_DEC,
+    .coding = MPP_VIDEO_CodingMJPEG,
+    .ctx_size = sizeof(JpegHalContext),
+    .flag = 0,
+    .init = hal_jpegd_init,
+    .deinit = hal_jpegd_deinit,
+    .reg_gen = hal_jpegd_reg_gen,
+    .start = hal_jpegd_start,
+    .wait = hal_jpegd_wait,
+    .reset = hal_jpegd_reset,
+    .flush = hal_jpegd_flush,
+    .control = hal_jpegd_control,
+};
---
+++ mpp/hal/vpu/jpegd/hal_jpegd_base.h
@@ -0,0 +1,45 @@
+/*
+ *
+ * Copyright 2015 - 2017 Rockchip Electronics Co. LTD
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef __HAL_JPEGD_BASE_H__
+#define __HAL_JPEGD_BASE_H__
+
+#include <stdio.h>
+#include "rk_type.h"
+#include "mpp_common.h"
+#include "mpp_mem.h"
+
+typedef struct JpegHalContext {
+    MppBufSlots packet_slots;
+    MppBufSlots frame_slots;
+    RK_S32      vpu_socket;
+    void        *regs;
+    MppBufferGroup group;
+    MppBuffer frame_buf;
+    MppBuffer pTableBase;
+    MppHalApi hal_api;
+
+    MppFrameFormat output_fmt;
+    RK_U32 set_output_fmt_flag;
+    RK_U32 hal_debug_enable;
+    RK_U32 frame_count;
+    RK_U32 output_yuv_count;
+
+    FILE *fp_reg_in;
+    FILE *fp_reg_out;
+} JpegHalContext;
+
+#endif /* __HAL_JPEGD_COMMON_H__ */
