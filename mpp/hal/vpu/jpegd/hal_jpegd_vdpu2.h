8d009228d93f90075673f45c8309055318c47361
Randy Li <randy.li@rock-chips.com>
2017-03-07 16:28:24 +0800

    [jpegd] rename to its device type
    It is a implementation for the VDPU2 video IP.
    
    Change-Id: Id2d83b971cf7e75c592ca5d8dd0b65d3c4e0122e
    Signed-off-by: Randy Li <randy.li@rock-chips.com>

---
+++ mpp/hal/vpu/jpegd/hal_jpegd_vdpu2.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2015 Rockchip Electronics Co. LTD
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __HAL_JPEGD_VDPU2_H__
+#define __HAL_JPEGD_VDPU2_H__
+#include "rk_type.h"
+
+MPP_RET hal_jpegd_vdpu2_init(void *hal, MppHalCfg *cfg);
+MPP_RET hal_jpegd_vdpu2_deinit(void *hal);
+MPP_RET hal_jpegd_vdpu2_gen_regs(void *hal,  HalTaskInfo *syn);
+MPP_RET hal_jpegd_vdpu2_start(void *hal, HalTaskInfo *task);
+MPP_RET hal_jpegd_vdpu2_wait(void *hal, HalTaskInfo *task);
+MPP_RET hal_jpegd_vdpu2_reset(void *hal);
+MPP_RET hal_jpegd_vdpu2_flush(void *hal);
+MPP_RET hal_jpegd_vdpu2_control(void *hal, RK_S32 cmd_type, void *param);
+
+#endif /* __HAL_JPEGD_VDPU2_H__ */
