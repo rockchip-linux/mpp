78a1778820e56f7c8a582adc09b692c24cea4b3f
Herman Chen <herman.chen@rock-chips.com>
2017-03-08 21:18:34 +0800

    [h264e]: Add intra task flag for output packet
    Change-Id: I55ee0bd825c77aa7172a89e86f40dfd406511c41
    Signed-off-by: Herman Chen <herman.chen@rock-chips.com>

--- mpp/codec/enc/h264/src/h264e_api.c
+++ mpp/codec/enc/h264/src/h264e_api.c
@@ -156,6 +156,7 @@ MPP_RET h264e_encode(void *ctx, HalEncTask *task)
     task->syntax.data   = &p->syntax;
     task->syntax.number = 1;
     task->valid = 1;
+    task->is_intra = (p->rc->cur_frmtype == INTRA_FRAME) ? (1) : (0);
 
     h264e_dbg_func("leave\n");
 
