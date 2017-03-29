3788fd2b197096dfb9173bd4524998cfab64b5ff
Randy Li <randy.li@rock-chips.com>
2017-03-29 11:10:56 +0800

    [pkgconfig]: offer a sample for pkgconfig
    The CMake could use this sample to generate the pkgconfig
    files for target system.
    
    Change-Id: I24c54533dc8f307b2e3748a2cce493a86cf274bc
    Signed-off-by: Randy Li <randy.li@rock-chips.com>

---
+++ pkgconfig/rockchip_mpp.pc.cmake
@@ -0,0 +1,12 @@
+prefix=/usr
+exec_prefix=${prefix}
+libdir=${prefix}/@CMAKE_INSTALL_LIBDIR@
+includedir=${prefix}/@CMAKE_INSTALL_INCLUDEDIR@
+
+Name: rockchip_mpp
+Description: Rockchip Media Process Platform
+Requires.private:
+Version: 1.3.0
+Libs: -L${libdir} -lrockchip_mpp
+Libs.private:
+Cflags: -I${includedir}
