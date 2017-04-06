44fd5433d0e553fdebdeb736858c2387eb3e77ac
Herman Chen <herman.chen@rock-chips.com>
2017-04-06 09:36:36 +0800

    [build]: Add build script for arm64
    Change-Id: I4ed1b62ce5baeb21fd8e9fb0d804d48f8d065698
    Signed-off-by: Herman Chen <herman.chen@rock-chips.com>

---
+++ build/linux/aarch64/arm.linux.cross.cmake
@@ -0,0 +1,12 @@
+
+cmake_minimum_required( VERSION 2.6.3 )
+
+SET(CMAKE_SYSTEM_NAME Linux)
+SET(CMAKE_C_COMPILER "aarch64-linux-gnu-gcc")
+SET(CMAKE_CXX_COMPILER "aarch64-linux-gnu-g++")
+#SET(CMAKE_SYSTEM_PROCESSOR "armv7-a")
+SET(CMAKE_SYSTEM_PROCESSOR "armv8")
+
+add_definitions(-fPIC)
+add_definitions(-DARMLINUX)
+
