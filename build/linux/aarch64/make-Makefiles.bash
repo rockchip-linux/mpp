76e5398633c69c0464ab9f74281caed83d8d8fb1
Herman Chen <herman.chen@rock-chips.com>
2017-04-06 09:36:36 +0800

    [build]: Add build script for arm64
    Change-Id: I4ed1b62ce5baeb21fd8e9fb0d804d48f8d065698
    Signed-off-by: Herman Chen <herman.chen@rock-chips.com>

---
+++ build/linux/aarch64/make-Makefiles.bash
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Run this from within a bash shell
+
+set +e
+
+MPP_PWD=`pwd`
+MPP_TOP=${MPP_PWD}/../../..
+
+# toolchain detection
+check_cmd(){
+    "$@" >> /dev/null 2>&1
+}
+check_system_arm_linux_gcc(){
+    check_cmd aarch64-linux-gnu-gcc -v
+}
+
+check_system_arm_linux_gcc
+if [ $? -eq 127 ];then
+    MPP_TOOLCHAIN=/usr/bin
+    export PATH=$PATH:${MPP_TOOLCHAIN}
+fi
+
+# generate Makefile
+cmake -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_TOOLCHAIN_FILE=./arm.linux.cross.cmake \
+      -DCMAKE_RKPLATFORM_ENABLE=ON \
+      -G "Unix Makefiles" \
+      ${MPP_TOP}
