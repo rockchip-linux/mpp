a2ef29193929095ac97fe65d65008b53b4744442
timkingh.huang <timkingh.huang@rock-chips.com>
2017-06-16 10:13:07 +0800

    [h264e]: limit bit rate on movement scene
    Change-Id: I6cc3398371883a7aab8aeea570dde8df043f3584
    Signed-off-by: timkingh.huang <timkingh.huang@rock-chips.com>

--- mpp/codec/inc/mpp_rc.h
+++ mpp/codec/inc/mpp_rc.h
@@ -144,6 +144,9 @@ typedef struct MppRateControl_s {
     RK_S32 last_fps_bits;
     float last_intra_percent;
 
+    RK_S32 prev_intra_target;
+    RK_S32 prev_inter_target;
+
     /* runtime status parameter */
     ENC_FRAME_TYPE cur_frmtype;
     ENC_FRAME_TYPE pre_frmtype;
