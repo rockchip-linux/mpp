b1ebee9af69a4ac737e4834ded544e8f8d7dd4fb
leo.ding <leo.ding@rock-chips.com>
2017-09-08 19:03:57 +0800

    [avsd]: add dpb error marking
    Change-Id: I733cebf27a42e9d360ee759a7a949cae6f7091c7
    Signed-off-by: leo.ding <leo.ding@rock-chips.com>

--- mpp/codec/dec/avs/avsd_parse.c
+++ mpp/codec/dec/avs/avsd_parse.c
@@ -536,6 +536,31 @@ MPP_RET avsd_set_dpb(AvsdCtx_t *p_dec, HalDecTask *task)
         }
     }
 
+    //!< set ref flag and mark error
+    if (p_dec->ph.picture_coding_type == I_PICTURE) {
+        task->flags.used_for_ref = 1;
+        task->flags.had_error = 0;
+    } else if (p_dec->ph.picture_coding_type == P_PICTURE) {
+        task->flags.used_for_ref = 1;
+        if (task->refer[0] >= 0) {
+            mpp_buf_slot_get_prop(p_dec->frame_slots, task->refer[0], SLOT_FRAME_PTR, &mframe);
+            if (mframe)
+                task->flags.had_error |= mpp_frame_get_errinfo(mframe);
+        }
+    } else if (p_dec->ph.picture_coding_type == B_PICTURE) {
+        task->flags.used_for_ref = 0;
+        if (task->refer[0] >= 0) {
+            mpp_buf_slot_get_prop(p_dec->frame_slots, task->refer[0], SLOT_FRAME_PTR, &mframe);
+            if (mframe)
+                task->flags.had_error |= mpp_frame_get_errinfo(mframe);
+        }
+        if (task->refer[1] >= 0) {
+            mpp_buf_slot_get_prop(p_dec->frame_slots, task->refer[1], SLOT_FRAME_PTR, &mframe);
+            if (mframe)
+                task->flags.had_error |= mpp_frame_get_errinfo(mframe);
+        }
+    }
+
     return MPP_OK;
 __FAILED:
     return MPP_NOK;
