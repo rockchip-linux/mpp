d65619436ec024f8790fa39c1ee77ab53159e6e5
Herman Chen <herman.chen@rock-chips.com>
2018-07-06 15:08:51 +0800

    [mpp_time]: Add MppTimer for time recording
    Change-Id: I3d04e677415ae84bc37703cb63a52be66c973798
    Signed-off-by: Herman Chen <herman.chen@rock-chips.com>

--- osal/inc/mpp_time.h
+++ osal/inc/mpp_time.h
@@ -30,12 +30,44 @@
 #define msleep(x)               usleep((x)*1000)
 #endif
 
+typedef void* MppTimer;
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 RK_S64 mpp_time();
 void mpp_time_diff(RK_S64 start, RK_S64 end, RK_S64 limit, const char *fmt);
+
+/*
+ * Timer create / destroy / enable / disable function
+ * Note when timer is create it is default disabled user need to call enable
+ * fucntion with enable = 1 to enable the timer.
+ * User can use enable function with enable = 0 to disable the timer.
+ */
+MppTimer mpp_timer_get(const char *name);
+void mpp_timer_put(MppTimer timer);
+void mpp_timer_enable(MppTimer timer, RK_U32 enable);
+
+/*
+ * Timer basic operation function:
+ * start : let timer start timing counter
+ * pause : let timer pause and return the diff to start time
+ * reset : let timer counter to all zero
+ */
+RK_S64 mpp_timer_start(MppTimer timer);
+RK_S64 mpp_timer_pause(MppTimer timer);
+RK_S64 mpp_timer_reset(MppTimer timer);
+
+/*
+ * These timer helper function can only be call when timer is paused:
+ * mpp_timer_get_sum    - Return timer sum up total value
+ * mpp_timer_get_count  - Return timer sum up counter value
+ * mpp_timer_get_name   - Return timer name
+ */
+RK_S64 mpp_timer_get_sum(MppTimer timer);
+RK_S64 mpp_timer_get_count(MppTimer timer);
+const char *mpp_timer_get_name(MppTimer timer);
 
 #ifdef __cplusplus
 }
