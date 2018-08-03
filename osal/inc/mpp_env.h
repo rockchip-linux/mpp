6d059f65bf23f912ec4ebb12022dd3b25b26dd6f
Herman Chen <herman.chen@rock-chips.com>
2018-08-03 15:56:03 +0800

    [osal]: Change mpp_env string interface
    Set get environment string parameter to const char to avoid modification.
    
    Change-Id: Ib5308407fcac2fa58b194fb8df33380efc1487f9
    Signed-off-by: Herman Chen <herman.chen@rock-chips.com>

--- osal/inc/mpp_env.h
+++ osal/inc/mpp_env.h
@@ -24,7 +24,7 @@ extern "C" {
 #endif
 
 RK_S32 mpp_env_get_u32(const char *name, RK_U32 *value, RK_U32 default_value);
-RK_S32 mpp_env_get_str(const char *name, char **value, char *default_value);
+RK_S32 mpp_env_get_str(const char *name, const char **value, const char *default_value);
 
 RK_S32 mpp_env_set_u32(const char *name, RK_U32 value);
 RK_S32 mpp_env_set_str(const char *name, char *value);
--- osal/mpp_env.cpp
+++ osal/mpp_env.cpp
@@ -24,7 +24,7 @@ RK_S32 mpp_env_get_u32(const char *name, RK_U32 *value, RK_U32 default_value)
     return os_get_env_u32(name, value, default_value);
 }
 
-RK_S32 mpp_env_get_str(const char *name, char **value, char *default_value)
+RK_S32 mpp_env_get_str(const char *name, const char **value, const char *default_value)
 {
     return os_get_env_str(name, value, default_value);
 }
