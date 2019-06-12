66b140eed828391994c04b8c53b1c1864c386576
Johnson Ding <johnson.ding@rock-chips.com>
2019-06-12 11:40:58 +0800

    [h264d]: Fix unused parameter warning
    Change-Id: Icf5233a95b3715bb4c7f61eb6f0961fe4d7da451
    Signed-off-by: Johnson Ding <johnson.ding@rock-chips.com>

--- mpp/codec/dec/h264/h264d_parse.c
+++ mpp/codec/dec/h264/h264d_parse.c
@@ -135,6 +135,8 @@ static void reset_nalu(H264dCurStream_t *p_strm)
 
 static void find_prefix_code(RK_U8 *p_data, H264dCurStream_t *p_strm)
 {
+    (void)p_data;
+
     if ((p_strm->prefixdata & 0x00FFFFFF) == 0x00000001) {
         if (p_strm->startcode_found) {
             p_strm->endcode_found = 1;
