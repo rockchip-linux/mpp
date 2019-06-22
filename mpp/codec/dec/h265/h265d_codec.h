b00f32fd4cd89a908d11933217bb81eca79088c0
Jonas Karlman <jonas@kwiboo.se>
2019-06-22 13:42:08 +0000

    [h265d]: fix color range and colorspace metadata
    Merged from
    https://github.com/Kwiboo/mpp/commit/f85d62a7d99f3da1d86ac57f94fa58c885ee5476
    
    Change-Id: I33ff292d3a484c4036900c58399f6c5e6e9c2faa
    Signed-off-by: Herman Chen <herman.chen@rock-chips.com>

--- mpp/codec/dec/h265/h265d_codec.h
+++ mpp/codec/dec/h265/h265d_codec.h
@@ -31,15 +31,17 @@
 #include "mpp_dec.h"
 
 enum MppColorSpace {
-    MPPCOL_SPC_RGB         = 0,
-    MPPCOL_SPC_BT709       = 1, ///< also ITU-R BT1361 / IEC 61966-2-4 xvYCC709 / SMPTE RP177 Annex B
-    MPPCOL_SPC_UNSPECIFIED = 2,
-    MPPCOL_SPC_FCC         = 4,
-    MPPCOL_SPC_BT470BG     = 5, ///< also ITU-R BT601-6 625 / ITU-R BT1358 625 / ITU-R BT1700 625 PAL & SECAM / IEC 61966-2-4 xvYCC601
-    MPPCOL_SPC_SMPTE170M   = 6, ///< also ITU-R BT601-6 525 / ITU-R BT1358 525 / ITU-R BT1700 NTSC / functionally identical to above
-    MPPCOL_SPC_SMPTE240M   = 7,
-    MPPCOL_SPC_YCOCG       = 8, ///< Used by Dirac / VC-2 and H.264 FRext, see ITU-T SG16
-    MPPCOL_SPC_NB             , ///< Not part of ABI
+    MPPCOL_SPC_RGB          = 0,
+    MPPCOL_SPC_BT709        = 1,    ///< also ITU-R BT1361 / IEC 61966-2-4 xvYCC709 / SMPTE RP177 Annex B
+    MPPCOL_SPC_UNSPECIFIED  = 2,
+    MPPCOL_SPC_FCC          = 4,
+    MPPCOL_SPC_BT470BG      = 5,    ///< also ITU-R BT601-6 625 / ITU-R BT1358 625 / ITU-R BT1700 625 PAL & SECAM / IEC 61966-2-4 xvYCC601
+    MPPCOL_SPC_SMPTE170M    = 6,    ///< also ITU-R BT601-6 525 / ITU-R BT1358 525 / ITU-R BT1700 NTSC / functionally identical to above
+    MPPCOL_SPC_SMPTE240M    = 7,
+    MPPCOL_SPC_YCOCG        = 8,    ///< Used by Dirac / VC-2 and H.264 FRext, see ITU-T SG16
+    MPPCOL_SPC_BT2020_NCL   = 9,    ///< ITU-R BT2020 non-constant luminance system
+    MPPCOL_SPC_BT2020_CL    = 10,   ///< ITU-R BT2020 constant luminance system
+    MPPCOL_SPC_NB             ,     ///< Not part of ABI
 };
 
 enum MppColorRange {
