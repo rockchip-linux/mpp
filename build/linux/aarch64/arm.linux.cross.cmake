451ae59386bbb8093a25defdca7a95f588c28706
syzby <huai_f@live.cn>
2019-08-22 13:58:37 +0800

    [cmake]: Fix aarch linux cmake compile error
    Change CMAKE_SYSTEM_PROCESSOR from armv8 to armv8-a
    
    Change-Id: I4e0021a44b933813d336383b5363170ecf2b7027
    Signed-off-by: Herman Chen <herman.chen@rock-chips.com>

--- build/linux/aarch64/arm.linux.cross.cmake
+++ build/linux/aarch64/arm.linux.cross.cmake
@@ -5,7 +5,7 @@ SET(CMAKE_SYSTEM_NAME Linux)
 SET(CMAKE_C_COMPILER "aarch64-linux-gnu-gcc")
 SET(CMAKE_CXX_COMPILER "aarch64-linux-gnu-g++")
 #SET(CMAKE_SYSTEM_PROCESSOR "armv7-a")
-SET(CMAKE_SYSTEM_PROCESSOR "armv8")
+SET(CMAKE_SYSTEM_PROCESSOR "armv8-a")
 
 add_definitions(-fPIC)
 add_definitions(-DARMLINUX)
