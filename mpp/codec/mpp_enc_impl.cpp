6785eca7b02f54b6f3654bc2062badf303b2fdca
Herman Chen <herman.chen@rock-chips.com>
2019-09-20 17:18:37 +0800

    [enc_impl]: Rename and new enc_impl function
    Change-Id: Ie0acfc1e66256db6acfba09e3c62b04ceda5e2a5
    Signed-off-by: Herman Chen <herman.chen@rock-chips.com>

--- mpp/codec/mpp_enc_impl.cpp
+++ mpp/codec/mpp_enc_impl.cpp
@@ -44,41 +44,26 @@ static const EncImplApi *controllers[] = {
 #endif
 };
 
-typedef struct ControllerImpl_t {
+typedef struct EncImplCtx_t {
     EncImplCfg          cfg;
     const EncImplApi    *api;
     void                *ctx;
-} ControllerImpl;
+} EncImplCtx;
 
-MPP_RET enc_impl_proc_hal(EncImpl ctrl, HalEncTask *task)
+MPP_RET enc_impl_init(EncImpl *impl, EncImplCfg *cfg)
 {
-    if (NULL == ctrl || NULL == task) {
-        mpp_err_f("found NULL input\n");
+    if (NULL == impl || NULL == cfg) {
+        mpp_err_f("found NULL input controller %p config %p\n", impl, cfg);
         return MPP_ERR_NULL_PTR;
     }
 
-    MPP_RET ret = MPP_OK;
-    ControllerImpl *p = (ControllerImpl *)ctrl;
-    if (p->api->proc_hal)
-        ret = p->api->proc_hal(p->ctx, task);
-
-    return ret;
-}
-
-MPP_RET enc_impl_init(EncImpl *ctrl, EncImplCfg *cfg)
-{
-    if (NULL == ctrl || NULL == cfg) {
-        mpp_err_f("found NULL input controller %p config %p\n", ctrl, cfg);
-        return MPP_ERR_NULL_PTR;
-    }
-
-    *ctrl = NULL;
+    *impl = NULL;
 
     RK_U32 i;
     for (i = 0; i < MPP_ARRAY_ELEMS(controllers); i++) {
         const EncImplApi *api = controllers[i];
         if (cfg->coding == api->coding) {
-            ControllerImpl *p = mpp_calloc(ControllerImpl, 1);
+            EncImplCtx *p = mpp_calloc(EncImplCtx, 1);
             void *ctx = mpp_calloc_size(void, api->ctx_size);
             if (NULL == ctx || NULL == p) {
                 mpp_err_f("failed to alloc parser context\n");
@@ -97,7 +82,7 @@ MPP_RET enc_impl_init(EncImpl *ctrl, EncImplCfg *cfg)
 
             p->api  = api;
             p->ctx  = ctx;
-            *ctrl = p;
+            *impl = p;
             return MPP_OK;
         }
     }
@@ -105,14 +90,14 @@ MPP_RET enc_impl_init(EncImpl *ctrl, EncImplCfg *cfg)
     return MPP_NOK;
 }
 
-MPP_RET enc_impl_deinit(EncImpl ctrl)
+MPP_RET enc_impl_deinit(EncImpl impl)
 {
-    if (NULL == ctrl) {
+    if (NULL == impl) {
         mpp_err_f("found NULL input\n");
         return MPP_ERR_NULL_PTR;
     }
 
-    ControllerImpl *p = (ControllerImpl *)ctrl;
+    EncImplCtx *p = (EncImplCtx *)impl;
     if (p->api->deinit)
         p->api->deinit(p->ctx);
 
@@ -121,32 +106,137 @@ MPP_RET enc_impl_deinit(EncImpl ctrl)
     return MPP_OK;
 }
 
-MPP_RET hal_enc_callback(void *ctrl, void *err_info)
+MPP_RET enc_impl_proc_cfg(EncImpl impl, RK_S32 cmd, void *para)
 {
-    if (NULL == ctrl) {
+    if (NULL == impl) {
         mpp_err_f("found NULL input\n");
         return MPP_ERR_NULL_PTR;
     }
 
     MPP_RET ret = MPP_OK;
-    ControllerImpl *p = (ControllerImpl *)ctrl;
-    if (p->api->callback)
-        ret = p->api->callback(p->ctx, err_info);
+    EncImplCtx *p = (EncImplCtx *)impl;
+    if (p->api->proc_cfg)
+        ret = p->api->proc_cfg(p->ctx, cmd, para);
 
     return ret;
 }
 
-MPP_RET enc_impl_proc_cfg(EncImpl ctrl, RK_S32 cmd, void *para)
+MPP_RET enc_impl_gen_hdr(EncImpl impl, MppPacket pkt)
 {
-    if (NULL == ctrl) {
+    if (NULL == impl) {
         mpp_err_f("found NULL input\n");
         return MPP_ERR_NULL_PTR;
     }
 
     MPP_RET ret = MPP_OK;
-    ControllerImpl *p = (ControllerImpl *)ctrl;
-    if (p->api->proc_cfg)
-        ret = p->api->proc_cfg(p->ctx, cmd, para);
+    EncImplCtx *p = (EncImplCtx *)impl;
+    if (p->api->gen_hdr)
+        ret = p->api->gen_hdr(p->ctx, pkt);
+
+    return ret;
+}
+
+MPP_RET enc_impl_proc_dpb(EncImpl impl)
+{
+    if (NULL == impl) {
+        mpp_err_f("found NULL input\n");
+        return MPP_ERR_NULL_PTR;
+    }
+
+    MPP_RET ret = MPP_OK;
+    EncImplCtx *p = (EncImplCtx *)impl;
+    if (p->api->proc_dpb)
+        ret = p->api->proc_dpb(p->ctx);
+
+    return ret;
+}
+
+MPP_RET enc_impl_proc_rc(EncImpl impl)
+{
+    if (NULL == impl) {
+        mpp_err_f("found NULL input\n");
+        return MPP_ERR_NULL_PTR;
+    }
+
+    MPP_RET ret = MPP_OK;
+    EncImplCtx *p = (EncImplCtx *)impl;
+    if (p->api->proc_rc)
+        ret = p->api->proc_rc(p->ctx);
+
+    return ret;
+}
+
+MPP_RET enc_impl_proc_hal(EncImpl impl, HalEncTask *task)
+{
+    if (NULL == impl || NULL == task) {
+        mpp_err_f("found NULL input\n");
+        return MPP_ERR_NULL_PTR;
+    }
+
+    MPP_RET ret = MPP_OK;
+    EncImplCtx *p = (EncImplCtx *)impl;
+    if (p->api->proc_hal)
+        ret = p->api->proc_hal(p->ctx, task);
+
+    return ret;
+}
+
+MPP_RET enc_impl_update_dpb(EncImpl impl)
+{
+    if (NULL == impl) {
+        mpp_err_f("found NULL input\n");
+        return MPP_ERR_NULL_PTR;
+    }
+
+    MPP_RET ret = MPP_OK;
+    EncImplCtx *p = (EncImplCtx *)impl;
+    if (p->api->update_dpb)
+        ret = p->api->update_dpb(p->ctx);
+
+    return ret;
+}
+
+MPP_RET enc_impl_update_hal(EncImpl impl, HalEncTask *task)
+{
+    if (NULL == impl || NULL == task) {
+        mpp_err_f("found NULL input\n");
+        return MPP_ERR_NULL_PTR;
+    }
+
+    MPP_RET ret = MPP_OK;
+    EncImplCtx *p = (EncImplCtx *)impl;
+    if (p->api->update_hal)
+        ret = p->api->update_hal(p->ctx, task);
+
+    return ret;
+}
+
+MPP_RET enc_impl_update_rc(EncImpl impl)
+{
+    if (NULL == impl) {
+        mpp_err_f("found NULL input\n");
+        return MPP_ERR_NULL_PTR;
+    }
+
+    MPP_RET ret = MPP_OK;
+    EncImplCtx *p = (EncImplCtx *)impl;
+    if (p->api->update_rc)
+        ret = p->api->update_rc(p->ctx);
+
+    return ret;
+}
+
+MPP_RET hal_enc_callback(void *impl, void *err_info)
+{
+    if (NULL == impl) {
+        mpp_err_f("found NULL input\n");
+        return MPP_ERR_NULL_PTR;
+    }
+
+    MPP_RET ret = MPP_OK;
+    EncImplCtx *p = (EncImplCtx *)impl;
+    if (p->api->callback)
+        ret = p->api->callback(p->ctx, err_info);
 
     return ret;
 }
