f54abf93113e25ee9ec7f5d2380ff75364fbc3ff
Herman Chen <herman.chen@rock-chips.com>
2019-11-18 19:39:21 +0800

    [mpp_enc_cfg]: Add frame status to enc_cfg.h
    1. EncFrameStatus will be used in dpb, rc and hal config.
    2. Remove misc config.
    
    Change-Id: I32eb4460180f3eb0b1c866c3393ed6fc4b95b99b
    Signed-off-by: Herman Chen <herman.chen@rock-chips.com>

--- mpp/inc/mpp_enc_cfg.h
+++ mpp/inc/mpp_enc_cfg.h
@@ -19,13 +19,48 @@
 
 #include "rk_venc_cmd.h"
 
-typedef struct MppEncMiscCfg_t {
-    MppEncSliceSplit    split;
-    MppEncGopRef        gop_ref;
-    MppEncROICfg        roi;
-    MppEncOSDData       osd_data;
-    MppEncOSDPlt        osd_plt;
-} MppEncMiscCfg;
+typedef struct EncFrmStatus_t {
+    /*
+     * 0 - inter frame
+     * 1 - intra frame
+     */
+    RK_U32  is_intra    : 1;
+
+    /*
+     * Valid when is_intra is true
+     * 0 - normal intra frame
+     * 1 - IDR frame
+     */
+    RK_U32  is_idr      : 1;
+
+    /*
+     * 0 - mark as reference frame
+     * 1 - mark as non-refernce frame
+     */
+    RK_U32  is_non_ref  : 1;
+
+    /*
+     * Valid when is_non_ref is false
+     * 0 - mark as short-term reference frame
+     * 1 - mark as long-term refernce frame
+     */
+    RK_U32  is_lt_ref   : 1;
+    RK_U32  lt_idx      : 4;
+    RK_U32  temporal_id : 4;
+
+    /*
+     * distance between current frame and reference frame
+     */
+    RK_S32  ref_dist    : 16;
+
+    /*
+     * reencode flag and force pskip flag
+     */
+    RK_U32  reencode    : 1;
+    RK_U32  force_pskip : 1;
+
+    RK_U32  stuff       : 2;
+} EncFrmStatus;
 
 /*
  * MppEncCfgSet shows the relationship between different configuration
@@ -42,8 +77,11 @@ typedef struct MppEncCfgSet_t {
     // codec detail config
     MppEncCodecCfg      codec;
 
-    // misc extra config
-    MppEncMiscCfg       misc;
+    MppEncSliceSplit    split;
+    MppEncGopRef        gop_ref;
+    MppEncROICfg        roi;
+    MppEncOSDData       osd_data;
+    MppEncOSDPlt        osd_plt;
 } MppEncCfgSet;
 
 #endif /*__MPP_ENC_H__*/
