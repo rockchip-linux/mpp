06689f82104e1074fbe6c586a2057dd7877082f7
Herman Chen <herman.chen@rock-chips.com>
2019-11-27 10:00:38 +0800

    [rc_debug]: Add rate control module debug function
    Change-Id: I0e2728f9080ba4d8ec55635bc78dc15c9402c595
    Signed-off-by: Herman Chen <herman.chen@rock-chips.com>

---
+++ mpp/codec/enc/rc/rc_debug.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2016 Rockchip Electronics Co. LTD
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __RC_DEBUG_H__
+#define __RC_DEBUG_H__
+
+#include "mpp_log.h"
+
+#define RC_DBG_FUNCTION             (0x00000001)
+#define RC_DBG_FPS                  (0x00000010)
+#define RC_DBG_BPS                  (0x00000020)
+#define RC_DBG_RC                   (0x00000030)
+#define RC_DBG_CFG                  (0x00000100)
+#define RC_DBG_RECORD               (0x00001000)
+#define RC_DBG_VBV                  (0x00002000)
+
+#define rc_dbg(flag, fmt, ...)      _mpp_dbg(rc_debug, flag, fmt, ## __VA_ARGS__)
+#define rc_dbg_f(flag, fmt, ...)    _mpp_dbg_f(rc_debug, flag, fmt, ## __VA_ARGS__)
+
+#define rc_dbg_func(fmt, ...)       rc_dbg_f(RC_DBG_FUNCTION, fmt, ## __VA_ARGS__)
+#define rc_dbg_fps(fmt, ...)        rc_dbg(RC_DBG_FPS, fmt, ## __VA_ARGS__)
+#define rc_dbg_bps(fmt, ...)        rc_dbg(RC_DBG_BPS, fmt, ## __VA_ARGS__)
+#define rc_dbg_rc(fmt, ...)         rc_dbg(RC_DBG_RC, fmt, ## __VA_ARGS__)
+#define rc_dbg_cfg(fmt, ...)        rc_dbg(RC_DBG_CFG, fmt, ## __VA_ARGS__)
+#define rc_dbg_vbv(fmt, ...)        rc_dbg(RC_DBG_VBV, fmt, ## __VA_ARGS__)
+
+extern RK_U32 rc_debug;
+
+#endif /* __RC_DEBUG_H__ */
