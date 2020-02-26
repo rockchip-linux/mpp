a3ea4f3baf3de6a6546f423ee0800fdb8825ec64
Herman Chen <herman.chen@rock-chips.com>
2020-02-26 09:03:26 +0800

    [cmake]: change build script for x86_64
    Change-Id: I181d09afdd10eadbf78ba1e016990b606205e6eb
    Signed-off-by: Herman Chen <herman.chen@rock-chips.com>

--- build/linux/x86_64/make-Makefiles.bash
+++ build/linux/x86_64/make-Makefiles.bash
@@ -1,7 +1,12 @@
 #!/bin/bash
 # Run this from within a bash shell
 # x86_64 is for simulation do not enable RK platform
+
+set +e
+
 cmake -G "Unix Makefiles" \
     -DCMAKE_BUILD_TYPE=debug \
     -DRKPLATFORM=OFF \
-    ../../../  && ccmake  ../../../
+    ../../../
+
+cmake --build . -j
