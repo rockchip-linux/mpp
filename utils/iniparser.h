6273dda76114f4d991331f5d5f6fd4b3677a2ffa
Herman Chen <herman.chen@rock-chips.com>
2020-02-27 17:24:21 +0800

    [utils]: Update iniparser
    Change-Id: Id29dc65a732aa62f4614f1d602545c04e7de054b
    Signed-off-by: Herman Chen <herman.chen@rock-chips.com>

--- utils/iniparser.h
+++ utils/iniparser.h
@@ -232,6 +232,21 @@ long int iniparser_getlongint(const dictionary * d, const char * key, long int n
 
 /*-------------------------------------------------------------------------*/
 /**
+  @brief    Get the string associated to a key, convert to a double
+  @param    d Dictionary to search
+  @param    key Key string to look for
+  @param    notfound Value to return in case of error
+  @return   double
+
+  This function queries a dictionary for a key. A key as read from an
+  ini file is given as "section:key". If the key cannot be found,
+  the notfound value is returned.
+ */
+/*--------------------------------------------------------------------------*/
+double iniparser_getdouble(const dictionary * d, const char * key, double notfound);
+
+/*-------------------------------------------------------------------------*/
+/**
   @brief    Get the string associated to a key, convert to a boolean
   @param    d Dictionary to search
   @param    key Key string to look for
