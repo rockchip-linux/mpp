3c9a207eda813361f5a76e96735788aede6949d7
Johnson Ding <johnson.ding@rock-chips.com>
2020-03-10 15:13:28 +0800

    [jpege]: fix jpeg enc error
    Change-Id: I4b92f45e833316b137d297a6f58ab77508227c2f
    Signed-off-by: Johnson Ding <johnson.ding@rock-chips.com>

--- mpp/hal/vpu/jpege/hal_jpege_vepu1.c
+++ mpp/hal/vpu/jpege/hal_jpege_vepu1.c
@@ -428,9 +428,9 @@ MPP_RET hal_jpege_vepu1_start(void *hal, HalTaskInfo *task)
 {
     MPP_RET ret = MPP_OK;
     HalJpegeCtx *ctx = (HalJpegeCtx *)hal;
-    RK_U32 *cache = NULL;
     RK_U32 reg_size = sizeof(jpege_vepu1_reg_set);
-    RK_U32 extra_size = sizeof(RegExtraInfo);
+    RK_U32 extra_size = (ctx->ioctl_info.extra_info.count) ?
+                        (sizeof(RegExtraInfo)) : (0);
     RK_U32 reg_num = reg_size / sizeof(RK_U32);
     RK_U32 extra_num = extra_size / sizeof(RK_U32);
     RegExtraInfo *info = &ctx->ioctl_info.extra_info;
@@ -440,26 +440,31 @@ MPP_RET hal_jpege_vepu1_start(void *hal, HalTaskInfo *task)
 
     if (mpp_get_ioctl_version()) {
         ret = mpp_device_send_extra_info(ctx->dev_ctx, info);
+
         if (ret)
             return MPP_ERR_VPUHW;
+
+        ret = mpp_device_send_reg(ctx->dev_ctx, ctx->ioctl_info.regs, nregs);
     } else {
-        if (mpp_device_patch_is_valid(info))
+        RK_U32 *cache = NULL;
+        if (mpp_device_patch_is_valid(info)) {
             nregs += extra_num;
+            cache = mpp_malloc(RK_U32, reg_num + extra_num);
+
+            if (!cache) {
+                mpp_err_f("failed to malloc reg cache\n");
+                return MPP_NOK;
+            }
+
+            memcpy(cache, ctx->ioctl_info.regs, reg_size);
+            memcpy(cache + reg_num, &(ctx->ioctl_info.extra_info), extra_size);
+            ret = mpp_device_send_reg(ctx->dev_ctx, cache, nregs);
+            mpp_free(cache);
+        } else {
+            ret = mpp_device_send_reg(ctx->dev_ctx, ctx->ioctl_info.regs, nregs);
+        }
     }
 
-    cache = mpp_malloc(RK_U32, reg_size + extra_size);
-    if (!cache) {
-        mpp_err_f("failed to malloc reg cache\n");
-        return MPP_NOK;
-    }
-
-    memcpy(cache, ctx->ioctl_info.regs, reg_size);
-    memcpy(cache + reg_num, &(ctx->ioctl_info.extra_info), extra_size);
-    if (ctx->dev_ctx) {
-        ret = mpp_device_send_reg(ctx->dev_ctx, cache, nregs);
-    }
-
-    mpp_free(cache);
 
     hal_jpege_dbg_func("leave hal %p\n", hal);
     (void)ctx;
