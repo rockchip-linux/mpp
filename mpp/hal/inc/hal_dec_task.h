8dd3150d6afb48e4a17543022b561ca64d9da1db
Herman Chen <herman.chen@rock-chips.com>
2020-03-17 14:21:56 +0800

    [hal_task]: Separate hal_task to different ones
    hal_task will be separated to dec / enc / vproc hal_tasks.
    
    Change-Id: I67c45772ca998a25ec116d6f08f0584f62408ad1
    Signed-off-by: Herman Chen <herman.chen@rock-chips.com>

---
+++ mpp/hal/inc/hal_dec_task.h
@@ -0,0 +1,96 @@
+/*
+*
+* Copyright 2015 Rockchip Electronics Co. LTD
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#ifndef __HAL_DEC_TASK__
+#define __HAL_DEC_TASK__
+
+#include "hal_task_defs.h"
+
+#define MAX_DEC_REF_NUM     17
+
+typedef struct IOInterruptCB {
+    MPP_RET (*callBack)(void*, void*);
+    void   *opaque;
+} IOInterruptCB;
+
+typedef struct IOCallbackCtx_t {
+    RK_U32      device_id;
+    void        *task;
+    RK_U32      *regs;
+    RK_U32       hard_err;
+} IOCallbackCtx;
+
+typedef union HalDecTaskFlag_t {
+    RK_U32          val;
+    struct {
+        RK_U32      eos              : 1;
+        RK_U32      info_change      : 1;
+
+        /*
+         * Different error flags for task
+         *
+         * parse_err :
+         * When set it means fatal error happened at parsing stage
+         * This task should not enable hardware just output a empty frame with
+         * error flag.
+         *
+         * ref_err :
+         * When set it means current task is ok but it contains reference frame
+         * with error which will introduce error pixels to this frame.
+         *
+         * used_for_ref :
+         * When set it means this output frame will be used as reference frame
+         * for further decoding. When there is error on decoding this frame
+         * if used_for_ref is set then the frame will set errinfo flag
+         * if used_for_ref is cleared then the frame will set discard flag.
+         */
+        RK_U32      parse_err        : 1;
+        RK_U32      ref_err          : 1;
+        RK_U32      used_for_ref     : 1;
+
+        RK_U32      wait_done        : 1;
+    };
+} HalDecTaskFlag;
+
+typedef struct HalDecTask_t {
+    // set by parser to signal that it is valid
+    RK_U32          valid;
+    HalDecTaskFlag  flags;
+
+    // previous task hardware working status
+    // when hardware error happen status is not zero
+    RK_U32          prev_status;
+    // current tesk protocol syntax information
+    MppSyntax       syntax;
+
+    // packet need to be copied to hardware buffer
+    // parser will create this packet and mpp_dec will copy it to hardware bufffer
+    MppPacket       input_packet;
+
+    // current task input slot index
+    RK_S32          input;
+
+    RK_S32          reg_index;
+    // for test purpose
+    // current tesk output slot index
+    RK_S32          output;
+
+    // current task reference slot index, -1 for unused
+    RK_S32          refer[MAX_DEC_REF_NUM];
+} HalDecTask;
+
+#endif /* __HAL_DEC_TASK__ */
---
+++ mpp/hal/inc/hal_enc_task.h
@@ -0,0 +1,60 @@
+/*
+*
+* Copyright 2015 Rockchip Electronics Co. LTD
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#ifndef __HAL_ENC_TASK__
+#define __HAL_ENC_TASK__
+
+#include "hal_task_defs.h"
+
+#define HAL_ENC_TASK_ERR_INIT         0x00000001
+#define HAL_ENC_TASK_ERR_ALLOC        0x00000010
+#define HAL_ENC_TASK_ERR_EXTRAINFO    0x00000100
+#define HAL_ENC_TASK_ERR_GENREG       0x00001000
+#define HAL_ENC_TASK_ERR_START        0x00010000
+#define HAL_ENC_TASK_ERR_WAIT         0x00100000
+
+typedef struct HalEncTaskFlag_t {
+    RK_U32 err;
+} HalEncTaskFlag;
+
+typedef struct HalEncTask_t {
+    RK_U32          valid;
+    RK_U32          reencode;
+
+    // current tesk protocol syntax information
+    MppSyntax       syntax;
+    MppSyntax       hal_ret;
+
+    // current tesk output stream buffer
+    MppPacket       packet;
+    MppBuffer       output;
+    RK_U32          length;
+
+    // current tesk input slot buffer
+    MppFrame        frame;
+    MppBuffer       input;
+
+    // current mv info output buffer
+    MppBuffer       mv_info;
+
+    RK_U32          is_intra;
+    RK_S32          temporal_id;
+
+    HalEncTaskFlag  flags;
+} HalEncTask;
+
+#endif /* __HAL_ENC_TASK__ */
