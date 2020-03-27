ca09525fccc0a32ee506fafc9ac9cbc916c9468a
Herman Chen <herman.chen@rock-chips.com>
2020-03-27 16:14:35 +0800

    [h264e]: Add sei writer function
    Signed-off-by: Herman Chen <herman.chen@rock-chips.com>
    Change-Id: Ia960cd919f6e2f99f258196b4bc076caea26ae70

---
+++ mpp/codec/enc/h264/h264e_sei.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2015 Rockchip Electronics Co. LTD
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __H264E_SEI_H__
+#define __H264E_SEI_H__
+
+#include "mpp_packet.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+MPP_RET h264e_sei_to_packet(void *data, RK_S32 size, RK_S32 type, MppPacket packet);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __H264E_SEI_H__ */
