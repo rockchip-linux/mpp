681599f97f3cc8f41ce308d9aa7d5e05c2419889
Herman Chen <herman.chen@rock-chips.com>
2020-04-04 17:19:57 +0800

    [test]: Update utils for encoder test
    1. Move command parsing to util files.
    2. Support frame format and coding type detection.
    
    Signed-off-by: Herman Chen <herman.chen@rock-chips.com>
    Change-Id: Id1df67ba61943bb7aa1f02ccccaa932fb5ae9842

--- utils/mpi_enc_utils.c
+++ utils/mpi_enc_utils.c
@@ -20,7 +20,214 @@
 
 #include "mpp_mem.h"
 #include "mpp_log.h"
+
+#include "rk_mpi.h"
+#include "utils.h"
 #include "mpi_enc_utils.h"
+
+#define MAX_FILE_NAME_LENGTH        256
+
+MpiEncTestArgs *mpi_enc_test_cmd_get(void)
+{
+    MpiEncTestArgs *args = mpp_calloc(MpiEncTestArgs, 1);
+
+    return args;
+}
+
+MPP_RET mpi_enc_test_cmd_update_by_args(MpiEncTestArgs* cmd, int argc, char **argv)
+{
+    const char *opt;
+    const char *next;
+    RK_S32 optindex = 1;
+    RK_S32 handleoptions = 1;
+    MPP_RET ret = MPP_NOK;
+
+    if ((argc < 2) || (cmd == NULL))
+        return ret;
+
+    /* parse options */
+    while (optindex < argc) {
+        opt  = (const char*)argv[optindex++];
+        next = (const char*)argv[optindex];
+
+        if (handleoptions && opt[0] == '-' && opt[1] != '\0') {
+            if (opt[1] == '-') {
+                if (opt[2] != '\0') {
+                    opt++;
+                } else {
+                    handleoptions = 0;
+                    continue;
+                }
+            }
+
+            opt++;
+
+            switch (*opt) {
+            case 'i':
+                if (next) {
+                    size_t len = strnlen(next, MAX_FILE_NAME_LENGTH);
+                    if (len) {
+                        cmd->file_input = mpp_calloc(char, len + 1);
+                        strncpy(cmd->file_input, next, len);
+                        name_to_frame_format(cmd->file_input, &cmd->format);
+                    }
+                } else {
+                    mpp_err("input file is invalid\n");
+                    goto PARSE_OPINIONS_OUT;
+                }
+                break;
+            case 'o':
+                if (next) {
+                    size_t len = strnlen(next, MAX_FILE_NAME_LENGTH);
+                    if (len) {
+                        cmd->file_output = mpp_calloc(char, len + 1);
+                        strncpy(cmd->file_output, next, len);
+                        name_to_coding_type(cmd->file_output, &cmd->type);
+                    }
+                } else {
+                    mpp_log("output file is invalid\n");
+                    goto PARSE_OPINIONS_OUT;
+                }
+                break;
+            case 'w':
+                if (next) {
+                    cmd->width = atoi(next);
+                    if (!cmd->hor_stride)
+                        cmd->hor_stride = cmd->width;
+                } else {
+                    mpp_err("invalid input width\n");
+                    goto PARSE_OPINIONS_OUT;
+                }
+                break;
+            case 'h':
+                if (!next)
+                    goto PARSE_OPINIONS_OUT;
+
+                if ((*(opt + 1) != '\0') && !strncmp(opt, "help", 4)) {
+                    goto PARSE_OPINIONS_OUT;
+                } else if (next) {
+                    cmd->height = atoi(next);
+                    if (!cmd->ver_stride)
+                        cmd->ver_stride = cmd->height;
+                }
+                break;
+            case 'u':
+                if (next) {
+                    cmd->hor_stride = atoi(next);
+                } else {
+                    mpp_err("invalid input width\n");
+                    goto PARSE_OPINIONS_OUT;
+                }
+                break;
+            case 'v':
+                if (next) {
+                    cmd->ver_stride = atoi(next);
+                } else {
+                    mpp_log("input height is invalid\n");
+                    goto PARSE_OPINIONS_OUT;
+                }
+                break;
+            case 'f':
+                if (next) {
+                    cmd->format = (MppFrameFormat)atoi(next);
+                    ret = ((cmd->format >= MPP_FMT_YUV_BUTT && cmd->format < MPP_FRAME_FMT_RGB) ||
+                           cmd->format >= MPP_FMT_RGB_BUTT);
+                }
+
+                if (!next || ret) {
+                    mpp_err("invalid input format %d\n", cmd->format);
+                    goto PARSE_OPINIONS_OUT;
+                }
+                break;
+            case 't':
+                if (next) {
+                    cmd->type = (MppCodingType)atoi(next);
+                    ret = mpp_check_support_format(MPP_CTX_ENC, cmd->type);
+                }
+
+                if (!next || ret) {
+                    mpp_err("invalid input coding type\n");
+                    goto PARSE_OPINIONS_OUT;
+                }
+                break;
+            case 'n':
+                if (next) {
+                    cmd->num_frames = atoi(next);
+                } else {
+                    mpp_err("invalid input max number of frames\n");
+                    goto PARSE_OPINIONS_OUT;
+                }
+                break;
+            case 'g':
+                if (next) {
+                    cmd->gop_mode = atoi(next);
+                } else {
+                    mpp_err("invalid gop mode\n");
+                    goto PARSE_OPINIONS_OUT;
+                }
+                break;
+            case 'b':
+                if (next) {
+                    cmd->bps_target = atoi(next);
+                } else {
+                    mpp_err("invalid bit rate\n");
+                    goto PARSE_OPINIONS_OUT;
+                }
+                break;
+            case 'r':
+                if (next) {
+                    cmd->fps_out = atoi(next);
+                } else {
+                    mpp_err("invalid output frame rate\n");
+                    goto PARSE_OPINIONS_OUT;
+                }
+                break;
+            case 'l':
+                if (next) {
+                    cmd->loop_cnt = atoi(next);
+                } else {
+                    mpp_err("invalid loop count\n");
+                    goto PARSE_OPINIONS_OUT;
+                }
+                break;
+            default:
+                mpp_err("skip invalid opt %c\n", *opt);
+                break;
+            }
+
+            optindex++;
+        }
+    }
+
+    ret = MPP_OK;
+
+    /* check essential parameter */
+    if (cmd->type <= MPP_VIDEO_CodingAutoDetect) {
+        mpp_err("invalid type %d\n", cmd->type);
+        ret = MPP_NOK;
+    }
+    if (cmd->width <= 0 || cmd->height <= 0 ||
+        cmd->hor_stride <= 0 || cmd->ver_stride <= 0) {
+        mpp_err("invalid w:h [%d:%d] stride [%d:%d]\n",
+                cmd->width, cmd->height, cmd->hor_stride, cmd->ver_stride);
+        ret = MPP_NOK;
+    }
+
+PARSE_OPINIONS_OUT:
+    return ret;
+}
+
+MPP_RET mpi_enc_test_cmd_put(MpiEncTestArgs* cmd)
+{
+    if (NULL == cmd)
+        return MPP_OK;
+
+    MPP_FREE(cmd->file_input);
+    MPP_FREE(cmd->file_output);
+    MPP_FREE(cmd);
+
+    return MPP_OK;
+}
 
 MPP_RET mpi_enc_gen_osd_data(MppEncOSDData *osd_data, MppBuffer osd_buf, RK_U32 frame_cnt)
 {
@@ -64,3 +271,38 @@ MPP_RET mpi_enc_gen_osd_plt(MppEncOSDPlt *osd_plt, RK_U32 *table)
     return MPP_OK;
 }
 
+static OptionInfo mpi_enc_cmd[] = {
+    {"i",               "input_file",           "input bitstream file"},
+    {"o",               "output_file",          "output bitstream file, "},
+    {"w",               "width",                "the width of input picture"},
+    {"h",               "height",               "the height of input picture"},
+    {"f",               "format",               "the format of input picture"},
+    {"t",               "type",                 "output stream coding type"},
+    {"n",               "max frame number",     "max encoding frame number"},
+    {"g",               "gop_mode",             "gop reference mode"},
+    {"d",               "debug",                "debug flag"},
+    {"b",               "target bps",           "set tareget bps"},
+    {"r",               "output frame rate",    "set output frame rate"},
+    {"l",               "loop count",           "loop encoding times for each frame"},
+};
+
+MPP_RET mpi_enc_test_cmd_show_opt(MpiEncTestArgs* cmd)
+{
+    mpp_log("cmd parse result:\n");
+    mpp_log("input  file name: %s\n", cmd->file_input);
+    mpp_log("output file name: %s\n", cmd->file_output);
+    mpp_log("width      : %d\n", cmd->width);
+    mpp_log("height     : %d\n", cmd->height);
+    mpp_log("format     : %d\n", cmd->format);
+    mpp_log("type       : %d\n", cmd->type);
+
+    return MPP_OK;
+}
+
+void mpi_enc_test_help(void)
+{
+    mpp_log("usage: mpi_enc_test [options]\n");
+    show_options(mpi_enc_cmd);
+    mpp_show_support_format();
+    mpp_show_color_format();
+}
