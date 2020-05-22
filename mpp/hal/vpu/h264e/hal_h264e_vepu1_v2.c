0c5dc02ebdd861f42822cb5315e08486f9cc2730
Herman Chen <herman.chen@rock-chips.com>
2020-05-22 15:22:56 +0800

    [hal_h264e_vepu]: Fix hardware stream length error
    Hardware output stream length includes the 64-bit header aligned length.
    So we need to subtract aligned length to get the real hardware length.
    
    Change-Id: I78f16afa02bc4428613cafcca9b8bde9241ba997
    Signed-off-by: Herman Chen <herman.chen@rock-chips.com>

--- mpp/hal/vpu/h264e/hal_h264e_vepu1_v2.c
+++ mpp/hal/vpu/h264e/hal_h264e_vepu1_v2.c
@@ -237,7 +237,7 @@ static MPP_RET hal_h264e_vepu1_get_task_v2(void *hal, HalEncTask *task)
 
 static RK_S32 setup_output_packet(RK_U32 *reg, MppBuffer buf, RK_U32 offset)
 {
-    RK_U32 offset8 = offset & (~0xf);
+    RK_U32 offset8 = offset & (~0x7);
     RK_S32 fd = mpp_buffer_get_fd(buf);
     RK_U32 hdr_rem_msb = 0;
     RK_U32 hdr_rem_lsb = 0;
@@ -302,6 +302,8 @@ static MPP_RET hal_h264e_vepu1_gen_regs_v2(void *hal, HalEncTask *task)
 
     /* setup output address with offset */
     first_free_bit = setup_output_packet(reg, task->output, offset);
+    /* set extra byte for header */
+    hw_mbrc->out_strm_size = first_free_bit / 8;
 
     /*
      * The hardware needs only the value for luma plane, because
@@ -576,10 +578,11 @@ static void h264e_vepu1_get_mbrc(HalH264eVepuMbRc *mb_rc, H264eVpu1RegSet *reg)
     RK_U32 cpt_prev = 0;
     RK_U32 overflow = 0;
     RK_U32 cpt_idx = VEPU_REG_CHECKPOINT(0) / 4;
+    RK_U32 base = mb_rc->out_strm_size;
     RK_U32 *reg_val = reg->val;
 
     mb_rc->hw_status        = reg_val[VEPU_REG_INTERRUPT / 4];
-    mb_rc->out_strm_size    = reg_val[VEPU_REG_STR_BUF_LIMIT / 4] / 8;
+    mb_rc->out_strm_size    = reg_val[VEPU_REG_STR_BUF_LIMIT / 4] / 8 - base;
     mb_rc->qp_sum           = VEPU_REG_QP_SUM(reg_val[VEPU_REG_MAD_CTRL / 4]);
     mb_rc->less_mad_count   = VEPU_REG_MB_CNT_SET(reg_val[VEPU_REG_MB_CTRL / 4]);
     mb_rc->rlc_count        = VEPU_REG_RLC_SUM_OUT(reg_val[VEPU_REG_RLC_CTRL / 4]);
