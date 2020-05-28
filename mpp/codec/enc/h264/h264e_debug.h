26ae5578b536214fe4b140ac3179f712c5161c20
Herman Chen <herman.chen@rock-chips.com>
2020-05-28 11:34:09 +0800

    [h264e_debug]: Add h264e control debug flag
    Change-Id: I70cce37b21c4db155f6bbe42842e6d952afb4bc1
    Signed-off-by: Herman Chen <herman.chen@rock-chips.com>

--- mpp/codec/enc/h264/h264e_debug.h
+++ mpp/codec/enc/h264/h264e_debug.h
@@ -20,23 +20,25 @@
 #include "mpp_log.h"
 
 #define H264E_DBG_FUNCTION          (0x00000001)
-#define H264E_DBG_FLOW              (0x00000020)
-#define H264E_DBG_DETAIL            (0x00000040)
+#define H264E_DBG_FLOW              (0x00000002)
+#define H264E_DBG_CTRL              (0x00000004)
+#define H264E_DBG_DETAIL            (0x00000008)
 
-#define H264E_DBG_SPS               (0x00000100)
-#define H264E_DBG_PPS               (0x00000200)
-#define H264E_DBG_SLICE             (0x00000400)
-#define H264E_DBG_SEI               (0x00000800)
+#define H264E_DBG_SPS               (0x00000010)
+#define H264E_DBG_PPS               (0x00000020)
+#define H264E_DBG_SLICE             (0x00000040)
+#define H264E_DBG_SEI               (0x00000080)
 
-#define H264E_DBG_DPB               (0x00001000)
-#define H264E_DBG_LIST              (0x00002000)
-#define H264E_DBG_MMCO              (0x00004000)
+#define H264E_DBG_DPB               (0x00000100)
+#define H264E_DBG_LIST              (0x00000200)
+#define H264E_DBG_MMCO              (0x00000400)
 
 #define h264e_dbg(flag, fmt, ...)   _mpp_dbg(h264e_debug, flag, fmt, ## __VA_ARGS__)
 #define h264e_dbg_f(flag, fmt, ...) _mpp_dbg_f(h264e_debug, flag, fmt, ## __VA_ARGS__)
 
 #define h264e_dbg_func(fmt, ...)    h264e_dbg_f(H264E_DBG_FUNCTION, fmt, ## __VA_ARGS__)
 #define h264e_dbg_flow(fmt, ...)    h264e_dbg_f(H264E_DBG_FLOW, fmt, ## __VA_ARGS__)
+#define h264e_dbg_ctrl(fmt, ...)    h264e_dbg_f(H264E_DBG_CTRL, fmt, ## __VA_ARGS__)
 #define h264e_dbg_detail(fmt, ...)  h264e_dbg_f(H264E_DBG_DETAIL, fmt, ## __VA_ARGS__)
 
 #define h264e_dbg_sps(fmt, ...)     h264e_dbg_f(H264E_DBG_SPS, fmt, ## __VA_ARGS__)
