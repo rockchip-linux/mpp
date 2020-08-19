9e771d441a641aefa2b3dc7da816407f0d5d5646
Herman Chen <herman.chen@rock-chips.com>
2020-08-19 09:20:20 +0800

    [jpege]: Disable some log for jpeg encoder
    Change-Id: I3f062b68231f43dc6820d34fd6b01796af0c15eb
    Signed-off-by: Herman Chen <herman.chen@rock-chips.com>

--- mpp/codec/enc/jpeg/jpege_debug.h
+++ mpp/codec/enc/jpeg/jpege_debug.h
@@ -22,6 +22,7 @@
 #define JPEGE_DBG_FUNCTION          (0x00000001)
 #define JPEGE_DBG_INPUT             (0x00000010)
 #define JPEGE_DBG_OUTPUT            (0x00000020)
+#define JPEGE_DBG_CTRL              (0x00000040)
 
 #define jpege_dbg(flag, fmt, ...)   _mpp_dbg(jpege_debug, flag, fmt, ## __VA_ARGS__)
 #define jpege_dbg_f(flag, fmt, ...) _mpp_dbg_f(jpege_debug, flag, fmt, ## __VA_ARGS__)
@@ -29,6 +30,7 @@
 #define jpege_dbg_func(fmt, ...)    jpege_dbg_f(JPEGE_DBG_FUNCTION, fmt, ## __VA_ARGS__)
 #define jpege_dbg_input(fmt, ...)   jpege_dbg(JPEGE_DBG_INPUT, fmt, ## __VA_ARGS__)
 #define jpege_dbg_output(fmt, ...)  jpege_dbg(JPEGE_DBG_OUTPUT, fmt, ## __VA_ARGS__)
+#define jpege_dbg_ctrl(fmt, ...)    jpege_dbg(JPEGE_DBG_CTRL, fmt, ## __VA_ARGS__)
 
 extern RK_U32 jpege_debug;
 
