9c4f863097b285a7762ac9c7675f4b56bf28508c
Ding Wei <leo.ding@rock-chips.com>
2020-08-20 16:31:23 +0800

    [h265d]: fix issue when parser_nal_unit return fail
    Signed-off-by: Ding Wei <leo.ding@rock-chips.com>
    Change-Id: I0dc47d9e9571d3f58342a45918a6eec6ec4372d2

--- mpp/codec/dec/h265/h265d_parser.c
+++ mpp/codec/dec/h265/h265d_parser.c
@@ -1594,6 +1594,10 @@ static RK_S32 parser_nal_units(HEVCContext *s)
 
     for (i = 0; i < s->nb_nals; i++) {
         ret = parser_nal_unit(s, s->nals[i].data, s->nals[i].size);
+        if (ret < 0) {
+            mpp_err("Error parsing NAL unit #%d,error ret = 0xd.\n", i, ret);
+            goto fail;
+        }
         /* update slice data if slice_header_extension_present_flag is 1*/
         if (s->nal_unit_type < 32) {
             switch (s->nal_unit_type) {
@@ -1623,10 +1627,6 @@ static RK_S32 parser_nal_units(HEVCContext *s)
             default: break;
             }
             slice_cnt++;
-        }
-        if (ret < 0) {
-            mpp_err("Error parsing NAL unit #%d,error ret = 0xd.\n", i, ret);
-            goto fail;
         }
     }
 fail:
