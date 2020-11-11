e82e4e017114cc8071d68d16b53aaeef9f87b8ed
Herman Chen <herman.chen@rock-chips.com>
2020-11-11 15:00:26 +0800

    [hal_h265d]: Add new hal_h265d_api module
    1. Add hal_h265d_api module to separate different decoder.
    2. Move some common definition to hal_h265d_com.
    3. Add new api header and register header for rkvdec H.265 decoder.
    4. Add unified debug header.
    
    Change-Id: If528f9a444790ec95c604eaf5e5f2f8822896da4
    Signed-off-by: yandong.lin <yandong.lin@rock-chips.com>
    Signed-off-by: Ding Wei <leo.ding@rock-chips.com>
    Signed-off-by: Herman Chen <herman.chen@rock-chips.com>

---
+++ mpp/hal/rkdec/h265d/hal_h265d_debug.h
@@ -0,0 +1,44 @@
+/*
+*
+* Copyright 2015 Rockchip Electronics Co. LTD
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#ifndef __HAL_H265D_DEBUG_H__
+#define __HAL_H265D_DEBUG_H__
+
+#include "rk_type.h"
+#include "mpp_err.h"
+#include "mpp_log.h"
+
+#define H265H_DBG_FUNCTION          (0x00000001)
+#define H265H_DBG_RPS               (0x00000002)
+#define H265H_DBG_PPS               (0x00000004)
+#define H265H_DBG_REG               (0x00000008)
+#define H265H_DBG_FAST_ERR          (0x00000010)
+#define H265H_DBG_TASK_ERR          (0x00000020)
+
+#define h265h_dbg(flag, fmt, ...) _mpp_dbg(hal_h265d_debug, flag, fmt, ## __VA_ARGS__)
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern RK_U32 hal_h265d_debug;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /*__HAL_H265D_DEBUG_H__*/
--- mpp/hal/rkdec/h265d/hal_h265d_rkv_reg.h
+++ mpp/hal/rkdec/h265d/hal_h265d_rkv_reg.h
@@ -27,15 +27,9 @@
 
 #ifndef __HAL_H265D_REG_H__
 #define __HAL_H265D_REG_H__
+
 #include "rk_type.h"
 #include "mpp_log.h"
-
-#define H265H_DBG_FUNCTION          (0x00000001)
-#define H265H_DBG_RPS               (0x00000002)
-#define H265H_DBG_PPS               (0x00000004)
-#define H265H_DBG_REG               (0x00000008)
-#define H265H_DBG_FAST_ERR          (0x00000010)
-#define H265H_DBG_TASK_ERR          (0x00000020)
 
 #define HEVC_DECODER_REG_NUM        (48)
 #define RKVDEC_REG_PERF_CYCLE_INDEX (64)
@@ -43,10 +37,6 @@
 #define RKVDEC_HEVC_REGISTERS       (68)
 #define RKVDEC_V1_REGISTERS         (78)
 #define V345_HEVC_REGISTERS         (108)
-
-#define h265h_dbg(flag, fmt, ...) _mpp_dbg(h265h_debug, flag, fmt, ## __VA_ARGS__)
-
-extern RK_U32 h265h_debug;
 
 typedef struct RKV_HEVC_REG_END {
     RK_U32        performance_cycle; //65
