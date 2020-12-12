ff2f267c1e11930822fee90898b5d66f824347bb
Herman Chen <herman.chen@rock-chips.com>
2020-12-12 10:06:17 +0800

    [hal_jpege]: Add more debug log macro
    Change-Id: Iee603d74a6ebead30193f0b30c47297853501651
    Signed-off-by: Herman Chen <herman.chen@rock-chips.com>

--- mpp/hal/vpu/jpege/hal_jpege_debug.h
+++ mpp/hal/vpu/jpege/hal_jpege_debug.h
@@ -20,6 +20,8 @@
 #include "mpp_log.h"
 
 #define HAL_JPEGE_DBG_FUNCTION          (0x00000001)
+#define HAL_JPEGE_DBG_SIMPLE            (0x00000002)
+#define HAL_JPEGE_DBG_DETAIL            (0x00000004)
 #define HAL_JPEGE_DBG_INPUT             (0x00000010)
 #define HAL_JPEGE_DBG_OUTPUT            (0x00000020)
 
@@ -27,6 +29,8 @@
 #define hal_jpege_dbg_f(flag, fmt, ...) _mpp_dbg_f(hal_jpege_debug, flag, fmt, ## __VA_ARGS__)
 
 #define hal_jpege_dbg_func(fmt, ...)    hal_jpege_dbg_f(HAL_JPEGE_DBG_FUNCTION, fmt, ## __VA_ARGS__)
+#define hal_jpege_dbg_simple(fmt, ...)  hal_jpege_dbg(HAL_JPEGE_DBG_SIMPLE, fmt, ## __VA_ARGS__)
+#define hal_jpege_dbg_detail(fmt, ...)  hal_jpege_dbg(HAL_JPEGE_DBG_DETAIL, fmt, ## __VA_ARGS__)
 #define hal_jpege_dbg_input(fmt, ...)   hal_jpege_dbg(HAL_JPEGE_DBG_INPUT, fmt, ## __VA_ARGS__)
 #define hal_jpege_dbg_output(fmt, ...)  hal_jpege_dbg(HAL_JPEGE_DBG_OUTPUT, fmt, ## __VA_ARGS__)
 
