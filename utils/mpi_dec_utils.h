eaf1e8f76beee36899b5c146e626b8f9cd3603ab
Yandong Lin <yandong.lin@rock-chips.com>
2020-12-31 10:22:23 +0800

    [mpi_dec_test]: Organize the common func of each dec test
    1. Set these func into mpi_dec_utils
    2. Note: num of threads: -n -> -s
    
    Signed-off-by: Yandong Lin <yandong.lin@rock-chips.com>
    Change-Id: If733fb0fdd7110313d2e140d3c50aad37260b6d2

---
+++ utils/mpi_dec_utils.h
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2020 Rockchip Electronics Co. LTD
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __MPI_DEC_UTILS_H__
+#define __MPI_DEC_UTILS_H__
+
+#include <stdio.h>
+#include "utils.h"
+
+#define MAX_FILE_NAME_LENGTH        256
+#define MPI_DEC_STREAM_SIZE         (SZ_4K)
+#define MPI_DEC_LOOP_COUNT          4
+
+/* For overall configure setup */
+typedef struct {
+    char            file_input[MAX_FILE_NAME_LENGTH];
+    char            file_output[MAX_FILE_NAME_LENGTH];
+    char            file_config[MAX_FILE_NAME_LENGTH];
+    MppCodingType   type;
+    MppFrameFormat  format;
+    RK_U32          width;
+    RK_U32          height;
+    RK_U32          debug;
+
+    RK_U32          have_input;
+    RK_U32          have_output;
+    RK_U32          have_config;
+
+    RK_U32          simple;
+    RK_S32          timeout;
+    RK_S32          frame_num;
+    size_t          pkt_size;
+    /* use for mpi_dec_multi_test */
+    RK_S32          nthreads;
+    // report information
+    size_t          max_usage;
+} MpiDecTestCmd;
+
+extern OptionInfo mpi_dec_cmd[];
+
+RK_S32  mpi_dec_test_parse_options(int argc, char **argv, MpiDecTestCmd* cmd);
+void    mpi_dec_test_show_options(MpiDecTestCmd* cmd);
+void    mpi_dec_test_help();
+
+#endif
