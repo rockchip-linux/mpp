a26e71291ea25c4df5d88ff4253b2ea1c02ba5b5
Chen Jinsen <kevin.chen@rock-chips.com>
2021-03-13 15:59:47 +0800

    [vpu_api_legacy]: support fbc fmt setup
    Signed-off-by: Chen Jinsen <kevin.chen@rock-chips.com>
    Change-Id: If43694ce50a0f163776a260a1a919d2f9d12a4d0

--- inc/vpu_api.h
+++ inc/vpu_api.h
@@ -92,6 +92,7 @@ typedef enum VPU_API_CMD {
     VPU_API_GET_FRAME_INFO,
     VPU_API_SET_OUTPUT_BLOCK,
     VPU_API_GET_EOS_STATUS,
+    VPU_API_SET_OUTPUT_MODE,
 
     /* get sps/pps header */
     VPU_API_GET_EXTRA_INFO = 0x200,
