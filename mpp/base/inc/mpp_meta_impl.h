7242c36a3d23507366d2a6d8c9c64256f0ec4b45
Herman Chen <herman.chen@rock-chips.com>
2021-11-02 19:43:26 +0800

    [mpp_meta]: Improve performance
    Change-Id: If2177552e5b71cd8acf5bfdb3e0a2ae20af71b4d
    Signed-off-by: Herman Chen <herman.chen@rock-chips.com>

--- mpp/base/inc/mpp_meta_impl.h
+++ mpp/base/inc/mpp_meta_impl.h
@@ -27,6 +27,18 @@ typedef struct MppMetaDef_t {
     MppMetaType         type;
 } MppMetaDef;
 
+typedef struct MppMetaVal_t {
+    RK_U32              state;
+    union {
+        RK_S32          val_s32;
+        RK_S64          val_s64;
+        void            *val_ptr;
+        MppFrame        frame;
+        MppPacket       packet;
+        MppBuffer       buffer;
+    };
+} MppMetaVal;
+
 typedef struct MppMetaImpl_t {
     char                tag[MPP_TAG_SIZE];
     const char          *caller;
@@ -36,26 +48,8 @@ typedef struct MppMetaImpl_t {
     struct list_head    list_meta;
     struct list_head    list_node;
     RK_S32              node_count;
+    MppMetaVal          vals[];
 } MppMetaImpl;
-
-typedef union MppMetaVal_u {
-    RK_S32              val_s32;
-    RK_S64              val_s64;
-    void                *val_ptr;
-    MppFrame            frame;
-    MppPacket           packet;
-    MppBuffer           buffer;
-} MppMetaVal;
-
-typedef struct MppMetaNode_t {
-    struct list_head    list_meta;
-    struct list_head    list_node;
-    MppMetaImpl         *meta;
-    RK_S32              node_id;
-
-    RK_S32              type_id;
-    MppMetaVal          val;
-} MppMetaNode;
 
 #ifdef __cplusplus
 extern "C" {
@@ -63,7 +57,6 @@ extern "C" {
 
 RK_S32 mpp_meta_size(MppMeta meta);
 MPP_RET mpp_meta_inc_ref(MppMeta meta);
-MppMetaNode *mpp_meta_next_node(MppMeta meta);
 
 #ifdef __cplusplus
 }
