ac6f8b1384dffecf70d609f47b88f1fe646173e0
Yandong Lin <yandong.lin@rock-chips.com>
2021-11-09 10:40:56 +0800

    [h264e_vepu]: add cfg to disable mb rc
    Signed-off-by: Yandong Lin <yandong.lin@rock-chips.com>
    Change-Id: I71e72164756b23f181a67a8b8799b9867a0854fb

--- mpp/codec/mpp_enc_impl.cpp
+++ mpp/codec/mpp_enc_impl.cpp
@@ -637,6 +637,9 @@ MPP_RET mpp_enc_proc_hw_cfg(MppEncHwCfg *dst, MppEncHwCfg *src)
         if (change & MPP_ENC_HW_CFG_CHANGE_AQ_STEP_P)
             memcpy(dst->aq_step_p, src->aq_step_p, sizeof(dst->aq_step_p));
 
+        if (change & MPP_ENC_HW_CFG_CHANGE_MB_RC)
+            dst->mb_rc_disable = src->mb_rc_disable;
+
         if (dst->qp_delta_row < 0 || dst->qp_delta_row_i < 0) {
             mpp_err("invalid hw qp delta row [%d:%d]\n",
                     dst->qp_delta_row_i, dst->qp_delta_row);
