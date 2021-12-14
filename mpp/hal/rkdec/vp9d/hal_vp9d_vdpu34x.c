af13bcf90ddc73c28f6422f9fe485e64c4f9e0a9
Ding Wei <leo.ding@rock-chips.com>
2021-12-14 11:34:52 +0800

    [vdpu34x]: re-set gating reference register
    tips:
        In RK3588,the reg26 gating_e should set 0xffef, disable bufifd gating bit.
    
    Change-Id: I25f329d386e1b16d8ffca49bef45804ab756fea5
    Signed-off-by: Ding Wei <leo.ding@rock-chips.com>

--- mpp/hal/rkdec/vp9d/hal_vp9d_vdpu34x.c
+++ mpp/hal/rkdec/vp9d/hal_vp9d_vdpu34x.c
@@ -821,9 +821,13 @@ static MPP_RET hal_vp9d_vdpu34x_gen_regs(void *hal, HalTaskInfo *task)
     vp9_hw_regs->common.reg010.dec_e            = 1;
     vp9_hw_regs->common.reg011.dec_timeout_e    = 1;
     vp9_hw_regs->common.reg011.buf_empty_en     = 1;
+    vp9_hw_regs->common.reg011.dec_clkgate_e    = 1;
+    vp9_hw_regs->common.reg011.dec_e_strmd_clkgate_dis = 0;
+
     vp9_hw_regs->common.reg013.timeout_mode     = 1;
 
-    vp9_hw_regs->common.reg026.swreg_block_gating_e = 0xfffff;
+    vp9_hw_regs->common.reg026.swreg_block_gating_e =
+        (mpp_get_soc_type() == ROCKCHIP_SOC_RK3588) ? 0xfffef : 0xfffff;
     vp9_hw_regs->common.reg026.reg_cfg_gating_en = 1;
     vp9_hw_regs->common.reg032_timeout_threshold = 0x0fffffff;
 
