072a0d6f0d202c1cadc9e60f7630c85e95934fde
Herman Chen <herman.chen@rock-chips.com>
2022-04-08 14:30:10 +0800

    [hal_h264e]: Fix extra syntax error log for dpb
    https://github.com/HermanChen/mpp/issues/42
    
    Change-Id: I85298a2cece2166932519a7cacc22806b42d5092
    Signed-off-by: Herman Chen <herman.chen@rock-chips.com>

--- mpp/hal/vpu/h264e/hal_h264e_vepu1_v2.c
+++ mpp/hal/vpu/h264e/hal_h264e_vepu1_v2.c
@@ -163,6 +163,9 @@ static RK_U32 update_vepu1_syntax(HalH264eVepu1Ctx *ctx, MppSyntax *syntax)
             hal_h264e_dbg_detail("update pps");
             ctx->pps = desc->p;
         } break;
+        case H264E_SYN_DPB : {
+            hal_h264e_dbg_detail("update dpb");
+        } break;
         case H264E_SYN_SLICE : {
             hal_h264e_dbg_detail("update slice");
             ctx->slice = desc->p;
