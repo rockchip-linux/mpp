aa2ea174d3334a0ed44958f3f721790db78e6964
Herman Chen <herman.chen@rock-chips.com>
2022-04-26 11:39:34 +0800

    [hal_h265e_vepu580]: Fix crash on FBC debreath path
    Change-Id: I14cd9ea0aa8740c9357b8d5699f2111d10bb7f8a
    Signed-off-by: Herman Chen <herman.chen@rock-chips.com>

--- mpp/hal/rkenc/h265e/hal_h265e_vepu580.c
+++ mpp/hal/rkenc/h265e/hal_h265e_vepu580.c
@@ -1958,6 +1958,7 @@ static MPP_RET vepu580_h265e_use_pass1_patch(H265eV580RegSet *regs, H265eV580Hal
     reg_ctl->reg0012_dtrns_map.src_bus_edin = fmt->src_endian;
     reg_base->reg0198_src_fmt.src_cfmt = VEPU541_FMT_YUV420SP;
     reg_base->reg0198_src_fmt.out_fmt = 1;
+    reg_base->reg0203_src_proc.afbcd_en = 0;
     reg_base->reg0205_src_strd0.src_strd0 = stridey;
     reg_base->reg0206_src_strd1.src_strd1 = stridey;
     reg_base->reg0160_adr_src0 = mpp_buffer_get_fd(ctx->buf_pass1);
