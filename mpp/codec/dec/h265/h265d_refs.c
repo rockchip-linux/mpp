71f90922a0a33fcc45cce16c5d2b2dce7c89a9d3
sayon.chen <sayon.chen@rock-chips.com>
2022-06-15 15:45:51 +0800

    [rkv_dec]: Add fbc_hdr_stride to output frame
    Change-Id: Ia5ab37c86f396848e0cc28a0c824c040835e7977
    Signed-off-by: sayon.chen <sayon.chen@rock-chips.com>

--- mpp/codec/dec/h265/h265d_refs.c
+++ mpp/codec/dec/h265/h265d_refs.c
@@ -100,8 +100,11 @@ static HEVCFrame *alloc_frame(HEVCContext *s)
             mpp_slots_set_prop(s->slots, SLOTS_HOR_ALIGN, hor_align_64);
             mpp_frame_set_offset_x(frame->frame, 0);
             mpp_frame_set_offset_y(frame->frame, 4);
+
             if (*compat_ext_fbc_buf_size)
                 mpp_frame_set_ver_stride(frame->frame, s->h265dctx->coded_height + 16);
+
+            mpp_frame_set_fbc_hdr_stride(frame->frame, MPP_ALIGN(s->h265dctx->width, 64));
         }
 
         mpp_frame_set_errinfo(frame->frame, 0);
