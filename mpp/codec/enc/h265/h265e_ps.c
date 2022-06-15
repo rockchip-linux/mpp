a053db7241d8d382167003cf69e28394f677cf37
sayon.chen <sayon.chen@rock-chips.com>
2022-06-15 09:50:21 +0800

    [h265e]: Fix fractional frame rate issue
    Change-Id: If26781a4e16592eb0c350f1e6f2ddeeac28bbd86
    Signed-off-by: sayon.chen <sayon.chen@rock-chips.com>

--- mpp/codec/enc/h265/h265e_ps.c
+++ mpp/codec/enc/h265/h265e_ps.c
@@ -169,8 +169,8 @@ MPP_RET h265e_set_sps(H265eCtx *ctx, H265eSps *sps, H265eVps *vps)
     MppEncRcCfg *rc = &ctx->cfg->rc;
     MppEncRefCfg ref_cfg = ctx->cfg->ref_cfg;
     MppEncH265VuiCfg *vui = &codec->vui;
-    RK_S32 i_timebase_num = 1;
-    RK_S32 i_timebase_den = rc->fps_out_num / rc->fps_out_denorm;
+    RK_S32 i_timebase_num = rc->fps_out_denorm;
+    RK_S32 i_timebase_den = rc->fps_out_num;
     RK_U8  convertToBit[MAX_CU_SIZE + 1];
     RK_U32 maxCUDepth, minCUDepth, addCUDepth;
     RK_S32 pad[2] = {0};
