f095476ab0d4ffc947c04c2b126942b19fb87515
Hongjin Li <vic.hong@rock-chips.com>
2022-09-16 14:17:28 +0800

    [vp9_parser]: Added log for yuv support
    Change-Id: Ie2f9ca9370726e526cb2fdba578c96abc5c56dc8
    Signed-off-by: Hongjin Li <vic.hong@rock-chips.com>

--- mpp/codec/dec/vp9/vp9d_parser.c
+++ mpp/codec/dec/vp9/vp9d_parser.c
@@ -580,8 +580,9 @@ static RK_S32 read_colorspace_details(Vp9CodecContext *ctx)
             s->ss_v = mpp_get_bit1(&s->gb);
             vp9d_dbg(VP9D_DBG_HEADER, "subsampling_y %d", s->ss_v);
             s->extra_plane = 0;
-            if ((res = pix_fmt_for_ss[bits][s->ss_v][s->ss_h]) == MPP_FMT_YUV420SP) {
-                mpp_err("YUV 4:2:0 not supported in profile %d\n", ctx->profile);
+            res = pix_fmt_for_ss[bits][s->ss_v][s->ss_h];
+            if (res == MPP_FMT_YUV420SP || res < 0) {
+                mpp_err("YUV FMT %d not supported in profile %d\n", res, ctx->profile);
                 return MPP_ERR_STREAM;
             } else if (mpp_get_bit1(&s->gb)) {
                 s->extra_plane = 1;
