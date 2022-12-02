cb8fa73dbdac28e80f0cb7dfc5245555b3579299
Rimon Xu <rimon.xu@rock-chips.com>
2022-12-02 18:13:13 +0800

    [vpu_api_legacy]: Support input timeout control
    Signed-off-by: Rimon Xu <rimon.xu@rock-chips.com>
    Change-Id: I960d28a75f1ae9425bb0db54dc2e017102b7e6cb

--- inc/vpu_api.h
+++ inc/vpu_api.h
@@ -104,6 +104,7 @@ typedef enum VPU_API_CMD {
     VPU_API_DEC_EN_HDR_META,
     VPU_API_DEC_EN_MVC,
     VPU_API_DEC_EN_FBC_HDR_256_ODD,
+    VPU_API_SET_INPUT_BLOCK,
 
     /* set pkt/frm ready callback */
     VPU_API_SET_PKT_RDY_CB = 0x1100,
