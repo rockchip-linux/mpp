750cf95d8d289ea9c0f0ff3d55c2f5b172d83a14
Yandong Lin <yandong.lin@rock-chips.com>
2022-12-06 14:43:30 +0800

    [mpp_frame]: add fbc size api for mpp internal using
    Signed-off-by: Yandong Lin <yandong.lin@rock-chips.com>
    Change-Id: I17aafcc7fde528331bcad3773796d27e50644c5f

--- mpp/base/mpp_frame.cpp
+++ mpp/base/mpp_frame.cpp
@@ -289,4 +289,5 @@ MPP_FRAME_ACCESSORS(size_t, buf_size)
 MPP_FRAME_ACCESSORS(RK_U32, errinfo)
 MPP_FRAME_ACCESSORS(MppTask, task)
 MPP_FRAME_ACCESSORS(RK_U32, thumbnail_en)
+MPP_FRAME_ACCESSORS(size_t, fbc_size)
 
