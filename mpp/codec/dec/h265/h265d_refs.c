5d81d75ba81a3c749c83dc5f6766e7ffee318b83
Yandong Lin <yandong.lin@rock-chips.com>
2023-01-16 14:22:25 +0800

    [h265d]: fix ref err with CRA frame issue
    Flush frame when there is a frame in the dpb with poc > cur poc.
    
    Signed-off-by: Yandong Lin <yandong.lin@rock-chips.com>
    Change-Id: Ib3a1f0a8bcdf1fddfa1f5b3928ea1dfd43bd4635

--- mpp/codec/dec/h265/h265d_refs.c
+++ mpp/codec/dec/h265/h265d_refs.c
@@ -30,6 +30,7 @@
 #include "mpp_compat_impl.h"
 
 #include "h265d_parser.h"
+#include "h265d_api.h"
 
 #define HEVC_ALIGN(value, x)   ((value + (x-1)) & (~(x-1)))
 
@@ -141,7 +142,8 @@ static HEVCFrame *alloc_frame(HEVCContext *s)
             mpp_frame_set_hdr_dynamic_meta(frame->frame, s->hdr_dynamic_meta);
             s->hdr_dynamic = 0;
         }
-        h265d_dbg(H265D_DBG_GLOBAL, "w_stride %d h_stride %d\n", s->h265dctx->coded_width, s->h265dctx->coded_height);
+        h265d_dbg(H265D_DBG_GLOBAL, "poc %d w_stride %d h_stride %d\n",
+                  s->poc, s->h265dctx->coded_width, s->h265dctx->coded_height);
         ret = mpp_buf_slot_get_unused(s->slots, &frame->slot_index);
         mpp_assert(ret == MPP_OK);
         return frame;
@@ -208,11 +210,9 @@ static HEVCFrame *find_ref_idx(HEVCContext *s, int poc)
                 return ref;
         }
     }
-    mpp_err("Could not find ref with POC %d\n", poc);
+    mpp_err("cur_poc %d Could not find ref with POC %d\n", s->poc, poc);
     return NULL;
 }
-
-
 
 static void mark_ref(HEVCFrame *frame, int flag)
 {
@@ -319,6 +319,19 @@ RK_S32 mpp_hevc_frame_rps(HEVCContext *s)
     /* release any frames that are now unused */
     for (i = 0; i < MPP_ARRAY_ELEMS(s->DPB); i++) {
         mpp_hevc_unref_frame(s, &s->DPB[i], 0);
+    }
+    /*
+     * flush for CRA frame if there has frame poc > cur poc in dpb.
+     */
+    if (IS_CRA(s)) {
+        for (i = 0; i < MPP_ARRAY_ELEMS(s->DPB); i++) {
+            HEVCFrame *ref = &s->DPB[i];
+
+            if ((ref->slot_index != 0xff) && (ref->poc > s->poc)) {
+                h265d_flush(s->h265dctx);
+                break;
+            }
+        }
     }
 
     return 0;
