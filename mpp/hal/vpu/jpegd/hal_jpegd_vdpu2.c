c10da154bab9ef9c5c96bc9f402a1b80839477b8
xueman.ruan <xueman.ruan@rock-chips.com>
2023-08-11 18:03:20 +0800

    fix[hal_jpegd]: fix rgb out_fmt mismatch error
    Issue occurs in socs after RK3588.
    
    Change-Id: Ieedd45c7d3234fc19ab5372a0a35f4fa08570abb
    Signed-off-by: xueman.ruan <xueman.ruan@rock-chips.com>

--- mpp/hal/vpu/jpegd/hal_jpegd_vdpu2.c
+++ mpp/hal/vpu/jpegd/hal_jpegd_vdpu2.c
@@ -1035,10 +1035,10 @@ MPP_RET hal_jpegd_vdpu2_control(void *hal, MpiCmd cmd_type,
     case MPP_DEC_SET_OUTPUT_FORMAT: {
         JpegHalCtx->output_fmt = *((MppFrameFormat *)param);
         JpegHalCtx->set_output_fmt_flag = 1;
-        jpegd_dbg_hal("output_format:%d\n", JpegHalCtx->output_fmt);
+        jpegd_dbg_hal("output_format: 0x%x\n", JpegHalCtx->output_fmt);
 
         if (!MPP_FRAME_FMT_IS_YUV(JpegHalCtx->output_fmt) && !MPP_FRAME_FMT_IS_RGB(JpegHalCtx->output_fmt)) {
-            mpp_err_f("output format %d is invalid.\n", JpegHalCtx->output_fmt);
+            mpp_err_f("invalid output format 0x%x\n", JpegHalCtx->output_fmt);
             ret = MPP_ERR_VALUE;
         }
     } break;
