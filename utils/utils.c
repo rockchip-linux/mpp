9d2eb94c3f21ba056852731a7066afd12fe90550
Yanjun Liao <yanjun.liao@rock-chips.com>
2024-01-25 16:11:01 +0800

    fix[mpp_dump]: add YUV420SP_10BIT format dump
    Change-Id: I1e89dbfefb394b4e8b2fbaf4fb0516be3528dd24
    Signed-off-by: Yanjun Liao <yanjun.liao@rock-chips.com>

--- utils/utils.c
+++ utils/utils.c
@@ -44,6 +44,21 @@ void _show_options(int count, OptionInfo *options)
     }
 }
 
+static void rearrange_pix(RK_U8 *tmp_line, RK_U8 *base, RK_U32 n)
+{
+    RK_U16 * pix = (RK_U16 *)(tmp_line + n * 16);
+    RK_U16 * base_u16 = (RK_U16 *)(base + n * 10);
+
+    pix[0] =  base_u16[0] & 0x03FF;
+    pix[1] = (base_u16[0] & 0xFC00) >> 10 | (base_u16[1] & 0x000F) << 6;
+    pix[2] = (base_u16[1] & 0x3FF0) >> 4;
+    pix[3] = (base_u16[1] & 0xC000) >> 14 | (base_u16[2] & 0x00FF) << 2;
+    pix[4] = (base_u16[2] & 0xFF00) >> 8  | (base_u16[3] & 0x0003) << 8;
+    pix[5] = (base_u16[3] & 0x0FFC) >> 2;
+    pix[6] = (base_u16[3] & 0xF000) >> 12 | (base_u16[4] & 0x003F) << 4;
+    pix[7] = (base_u16[4] & 0xFFC0) >> 6;
+}
+
 void dump_mpp_frame_to_file(MppFrame frame, FILE *fp)
 {
     RK_U32 width    = 0;
@@ -137,37 +152,15 @@ void dump_mpp_frame_to_file(MppFrame frame, FILE *fp)
         }
 
         for (i = 0; i < height; i++, base_y += h_stride) {
-            for (k = 0; k < width / 8; k++) {
-                RK_U16 *pix = (RK_U16 *)(tmp_line + k * 16);
-                RK_U16 *base_u16 = (RK_U16 *)(base_y + k * 10);
-
-                pix[0] =  base_u16[0] & 0x03FF;
-                pix[1] = (base_u16[0] & 0xFC00) >> 10 | (base_u16[1] & 0x000F) << 6;
-                pix[2] = (base_u16[1] & 0x3FF0) >> 4;
-                pix[3] = (base_u16[1] & 0xC000) >> 14 | (base_u16[2] & 0x00FF) << 2;
-                pix[4] = (base_u16[2] & 0xFF00) >> 8  | (base_u16[3] & 0x0003) << 8;
-                pix[5] = (base_u16[3] & 0x0FFC) >> 2;
-                pix[6] = (base_u16[3] & 0xF000) >> 12 | (base_u16[4] & 0x003F) << 4;
-                pix[7] = (base_u16[4] & 0xFFC0) >> 6;
-            }
-            fwrite(tmp_line, width * sizeof(RK_U16) , 1, fp);
+            for (k = 0; k < MPP_ALIGN(width, 8) / 8; k++)
+                rearrange_pix(tmp_line, base_y, k);
+            fwrite(tmp_line, width * sizeof(RK_U16), 1, fp);
         }
 
         for (i = 0; i < height / 2; i++, base_c += h_stride) {
-            for (k = 0; k < (width / 8); k++) {
-                RK_U16 *pix = (RK_U16 *)(tmp_line + k * 16);
-                RK_U16 *base_u16 = (RK_U16 *)(base_c + k * 10);
-
-                pix[0] = base_u16[0] & 0x03FF;
-                pix[1] = (base_u16[0] & 0xFC00) >> 10 | (base_u16[1] & 0x000F) << 6;
-                pix[2] = (base_u16[1] & 0x3FF0) >> 4;
-                pix[3] = (base_u16[1] & 0xC000) >> 14 | (base_u16[2] & 0x00FF) << 2;
-                pix[4] = (base_u16[2] & 0xFF00) >> 8  | (base_u16[3] & 0x0003) << 8;
-                pix[5] = (base_u16[3] & 0x0FFC) >> 2;
-                pix[6] = (base_u16[3] & 0xF000) >> 12 | (base_u16[4] & 0x003F) << 4;
-                pix[7] = (base_u16[4] & 0xFFC0) >> 6;
-            }
-            fwrite(tmp_line, width * sizeof(RK_U16) , 1, fp);
+            for (k = 0; k < MPP_ALIGN(width, 8) / 8; k++)
+                rearrange_pix(tmp_line, base_c, k);
+            fwrite(tmp_line, width * sizeof(RK_U16), 1, fp);
         }
 
         MPP_FREE(tmp_line);
