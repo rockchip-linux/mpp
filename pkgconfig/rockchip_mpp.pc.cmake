c61393abc62eec26bf1df0e0d90d08875d9821db
Herman Chen <herman.chen@rock-chips.com>
2024-04-19 09:26:46 +0800

    [script]: Update pkgconfig version
    Signed-off-by: Herman Chen <herman.chen@rock-chips.com>
    Change-Id: I595cf5a913238400714d3174361586aa4479fbe9

--- pkgconfig/rockchip_mpp.pc.cmake
+++ pkgconfig/rockchip_mpp.pc.cmake
@@ -6,7 +6,7 @@ includedir=${prefix}/@CMAKE_INSTALL_INCLUDEDIR@
 Name: rockchip_mpp
 Description: Rockchip Media Process Platform
 Requires.private:
-Version: 1.3.8
+Version: 1.3.9
 Libs: -L${libdir} -lrockchip_mpp
 Libs.private:
 Cflags: -I${includedir}
--- pkgconfig/rockchip_vpu.pc.cmake
+++ pkgconfig/rockchip_vpu.pc.cmake
@@ -6,7 +6,7 @@ includedir=${prefix}/@CMAKE_INSTALL_INCLUDEDIR@
 Name: rockchip_vpu
 Description: Rockchip Media Process Platform (Legacy)
 Requires.private:
-Version: 0.3.0
+Version: 0.3.1
 Libs: -L${libdir} -lrockchip_vpu
 Libs.private:
 Cflags: -I${includedir}
