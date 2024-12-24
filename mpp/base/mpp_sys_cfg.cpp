146d667db50adf300e11209713cf0f50d73fa5d3
Yandong Lin <yandong.lin@rock-chips.com>
2024-12-24 15:53:35 +0800

    fix[mpp_sys_cfg]: Fix compile warning
    Compile linux/aarch64 with toolchain:
    gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu-gcc
    gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu-g++
    
    Compile message:
    mpp/base/mpp_sys_cfg.cpp: In constructor ‘MppSysCfgService::MppSysCfgService()’:
    mpp/base/mpp_sys_cfg.cpp:88:61: warning: enum constant in boolean context [-Wint-in-bool-context]
       88 |     ENTRY(dec_buf_chk, type,        U32, MppCodingType,     MPP_SYS_DEC_BUF_CHK_CFG_CHANGE_TYPE,            dec_buf_chk, type) \
          |                                                             ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    mpp/base/mpp_sys_cfg.cpp:77:13: note: in definition of macro ‘EXPAND_AS_TRIE’
       77 |             flag ? 1 : 0, \
          |             ^~~~
    mpp/base/mpp_sys_cfg.cpp:123:5: note: in expansion of macro ‘ENTRY_TABLE’
      123 |     ENTRY_TABLE(EXPAND_AS_TRIE)
    
    Change-Id: Id60d66fdf0fd7be75d577ca8fc9b45eee3eeb2dd
    Signed-off-by: Yandong Lin <yandong.lin@rock-chips.com>

--- mpp/base/mpp_sys_cfg.cpp
+++ mpp/base/mpp_sys_cfg.cpp
@@ -74,7 +74,7 @@ public:
     do { \
         MppCfgInfo tmp = { \
             CFG_FUNC_TYPE_##cfg_type, \
-            flag ? 1 : 0, \
+            flag > 0 ? 1 : 0, \
             (RK_U32)((long)&(((MppSysCfgSet *)0)->field_change.change)), \
             flag, \
             (RK_U32)((long)&(((MppSysCfgSet *)0)->field_change.field_data)), \
