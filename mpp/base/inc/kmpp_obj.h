5f76bef01819cf695f465a7fcd42bc1147e6296d
Yandong Lin <yandong.lin@rock-chips.com>
2025-01-02 15:06:53 +0800

    feat[kmpp_obj]: Add kmpp_obj_get_hnd func
    1. Add kmpp_obj_get_hnd for kernel ioctl.
    2. Add kmpp_obj_get_hnd_size for share handle copy size.
    
    Change-Id: I1acac5ebb4a89e97c547f1ba3f57bf3add5b4e14
    Signed-off-by: Yandong Lin <yandong.lin@rock-chips.com>

--- mpp/base/inc/kmpp_obj.h
+++ mpp/base/inc/kmpp_obj.h
@@ -33,50 +33,6 @@ typedef union MppLocTbl_u {
     };
 } KmppLocTbl;
 
-/* kernel object trie share info */
-/*
- * kernel object trie share info
- * used in KMPP_SHM_IOC_QUERY_INFO
- *
- * input  : object name userspace address
- * output : trie_root userspace address (read only)
- */
-typedef union KmppObjTrie_u {
-    __u64       name_uaddr;
-    __u64       trie_root;
-} KmppObjTrie;
-
-/* kernel object share memory userspace visable header data */
-typedef struct KmppObjShm_t {
-    /* kobj_uaddr   - the userspace base address for kernel object */
-    __u64       kobj_uaddr;
-    /* kobj_kaddr   - the kernel base address for kernel object */
-    __u64       kobj_kaddr;
-    /* DO NOT access reserved data only used by kernel */
-} KmppObjShm;
-
-/* kernel object share memory get / put ioctl data */
-typedef struct KmppObjIocArg_t {
-    /* address array element count */
-    __u32       count;
-
-    /* flag for batch operation */
-    __u32       flag;
-
-    /*
-     * at KMPP_SHM_IOC_GET_SHM
-     * name_uaddr   - kernel object name in userspace address
-     * kobj_uaddr   - kernel object userspace address for KmppObjShm
-     *
-     * at KMPP_SHM_IOC_PUT_SHM
-     * kobj_uaddr   - kernel object userspace address for KmppObjShm
-     */
-    union {
-        __u64   name_uaddr[0];
-        __u64   kobj_uaddr[0];
-    };
-} KmppObjIocArg;
-
 /* KmppObjDef - mpp object name size and access table trie definition */
 typedef void* KmppObjDef;
 /* KmppObj - mpp object for string name access and function access */
@@ -106,6 +62,14 @@ rk_s32 kmpp_obj_get(KmppObj *obj, KmppObjDef def);
 rk_s32 kmpp_obj_put(KmppObj obj);
 rk_s32 kmpp_obj_check(KmppObj obj, const char *caller);
 
+/* handle is the kernel share object userspace base address for kernel ioctl */
+void *kmpp_obj_get_hnd(KmppObj obj);
+/* handle size defined the copy size for kernel ioctl */
+rk_s32 kmpp_obj_get_hnd_size(KmppObj obj);
+/*
+ * entry is the userspace address for kernel share object body
+ * entry = handle + entry_offset
+ */
 void *kmpp_obj_get_entry(KmppObj obj);
 
 rk_s32 kmpp_obj_set_s32(KmppObj obj, const char *name, rk_s32 val);
--- mpp/base/kmpp_obj.c
+++ mpp/base/kmpp_obj.c
@@ -42,6 +42,50 @@
 
 #define ENTRY_TO_FLAG_PTR(tbl, entry)   ((rk_u16 *)((char *)entry + tbl->flag_offset))
 
+/* kernel object trie share info */
+/*
+ * kernel object trie share info
+ * used in KMPP_SHM_IOC_QUERY_INFO
+ *
+ * input  : object name userspace address
+ * output : trie_root userspace address (read only)
+ */
+typedef union KmppObjTrie_u {
+    __u64       name_uaddr;
+    __u64       trie_root;
+} KmppObjTrie;
+
+/* kernel object share memory userspace visable header data */
+typedef struct KmppObjShm_t {
+    /* kobj_uaddr   - the userspace base address for kernel object */
+    __u64       kobj_uaddr;
+    /* kobj_kaddr   - the kernel base address for kernel object */
+    __u64       kobj_kaddr;
+    /* DO NOT access reserved data only used by kernel */
+} KmppObjShm;
+
+/* kernel object share memory get / put ioctl data */
+typedef struct KmppObjIocArg_t {
+    /* address array element count */
+    __u32       count;
+
+    /* flag for batch operation */
+    __u32       flag;
+
+    /*
+     * at KMPP_SHM_IOC_GET_SHM
+     * name_uaddr   - kernel object name in userspace address
+     * kobj_uaddr   - kernel object userspace address for KmppObjShm
+     *
+     * at KMPP_SHM_IOC_PUT_SHM
+     * kobj_uaddr   - kernel object userspace address for KmppObjShm
+     */
+    union {
+        __u64   name_uaddr[0];
+        __u64   kobj_uaddr[0];
+    };
+} KmppObjIocArg;
+
 typedef struct KmppObjDefImpl_t {
     struct list_head list;
     MppTrie trie;
@@ -514,6 +558,24 @@ rk_s32 kmpp_obj_check(KmppObj obj, const char *caller)
     }
 
     return MPP_OK;
+}
+
+void *kmpp_obj_get_hnd(KmppObj obj)
+{
+    KmppObjImpl *impl = (KmppObjImpl *)obj;
+
+    if (!impl) {
+        mpp_loge("invalid obj %p\n", obj);
+        return NULL;
+    }
+
+    return impl->shm;
+}
+
+rk_s32 kmpp_obj_get_hnd_size(KmppObj obj)
+{
+    (void)obj;
+    return sizeof(KmppObjShm);
 }
 
 void *kmpp_obj_get_entry(KmppObj obj)
