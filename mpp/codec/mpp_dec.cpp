dfd2763689381c039fd6b5680586a15c31b338bc
Hongjin Li <vic.hong@rock-chips.com>
2025-01-10 09:40:41 +0800

    fix[fbc]: Fix RK3588 av1 FBC usage issue
    Platform: RK3588
    
    Error case:
    1. After setting FBC, the current platform was not checked for FBC support.
    2. The RK3588 AV1 supports FBC.
    
    Signed-off-by: Hongjin Li <vic.hong@rock-chips.com>
    Change-Id: I0696c4469d514291547ed21a80089812c7e6b585

--- mpp/codec/mpp_dec.cpp
+++ mpp/codec/mpp_dec.cpp
@@ -146,6 +146,7 @@ MPP_RET mpp_dec_proc_cfg(MppDecImpl *dec, MpiCmd cmd, void *param)
     case MPP_DEC_SET_DISABLE_DPB_CHECK: {
         ret = mpp_dec_set_cfg_by_cmd(&dec->cfg, cmd, param);
         mpp_dec_update_cfg(dec);
+        mpp_dec_check_fbc_cap(dec);
         dec->cfg.base.change = 0;
     } break;
     case MPP_DEC_QUERY: {
