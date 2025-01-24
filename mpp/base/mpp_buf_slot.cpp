e492233ad8f8ccf05be0735ad71b1d4107592eea
Hongjin Li <vic.hong@rock-chips.com>
2025-01-24 14:19:48 +0800

    fix[buf_slot]: Correct coding mistakes.
    Signed-off-by: Hongjin Li <vic.hong@rock-chips.com>
    Change-Id: I4ca52cf44f09ffa08f4a73df1651d1881a636ccb

--- mpp/base/mpp_buf_slot.cpp
+++ mpp/base/mpp_buf_slot.cpp
@@ -497,10 +497,10 @@ static void generate_info_set(MppBufSlotsImpl *impl, MppFrame frame, RK_U32 forc
     info_set_impl->chroma_location  = frame_impl->chroma_location;
 
     if (buf_slot_debug & BUF_SLOT_DBG_INFO_SET) {
-        mpp_assert(sys_cfg_info_set.h_stride_by_pixel == sys_cfg_info_set.h_stride_by_pixel);
-        mpp_assert(sys_cfg_info_set.h_stride_by_byte == sys_cfg_info_set.h_stride_by_byte);
-        mpp_assert(sys_cfg_info_set.v_stride == sys_cfg_info_set.v_stride);
-        mpp_assert(sys_cfg_info_set.size_total == sys_cfg_info_set.size_total);
+        mpp_assert(legacy_info_set.h_stride_by_pixel == sys_cfg_info_set.h_stride_by_pixel);
+        mpp_assert(legacy_info_set.h_stride_by_byte == sys_cfg_info_set.h_stride_by_byte);
+        mpp_assert(legacy_info_set.v_stride == sys_cfg_info_set.v_stride);
+        mpp_assert(legacy_info_set.size_total == sys_cfg_info_set.size_total);
     }
 }
 
