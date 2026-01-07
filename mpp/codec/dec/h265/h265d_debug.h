4b5d503db6fab002d6ad976b2a15c51b1949ad70
Herman Chen <herman.chen@rock-chips.com>
2026-01-07 09:15:35 +0800

    chore[h265d_debug]: Add more debug flag
    Signed-off-by: Herman Chen <herman.chen@rock-chips.com>
    Change-Id: I9ef87cd1c85b1858ab923629f485cea1915ee539

--- mpp/codec/dec/h265/h265d_debug.h
+++ mpp/codec/dec/h265/h265d_debug.h
@@ -11,19 +11,23 @@
 extern RK_U32 h265d_debug;
 
 #define H265D_DBG_FUNCTION          (0x00000001)
-#define H265D_DBG_VPS               (0x00000002)
-#define H265D_DBG_SPS               (0x00000004)
-#define H265D_DBG_PPS               (0x00000008)
-#define H265D_DBG_SLICE             (0x00000010)
-#define H265D_DBG_SEI               (0x00000020)
-#define H265D_DBG_GLOBAL            (0x00000040)
-#define H265D_DBG_REF               (0x00000080)
-#define H265D_DBG_TIME              (0x00000100)
-#define H265D_DBG_DETAIL            (0x00000200)
+#define H265D_DBG_SPLIT             (0x00000002)
+#define H265D_DBG_PTS               (0x00000004)
+
+#define H265D_DBG_VPS               (0x00000010)
+#define H265D_DBG_SPS               (0x00000020)
+#define H265D_DBG_PPS               (0x00000040)
+#define H265D_DBG_SLICE             (0x00000080)
+#define H265D_DBG_SEI               (0x00000100)
+#define H265D_DBG_GLOBAL            (0x00001000)
+#define H265D_DBG_REF               (0x00002000)
+#define H265D_DBG_DETAIL            (0x00004000)
 
 #define h265d_dbg(flag, fmt, ...)   mpp_dbg(h265d_debug, flag, fmt, ## __VA_ARGS__)
 
 #define h265d_dbg_func(fmt, ...)    h265d_dbg(H265D_DBG_FUNCTION, fmt, ## __VA_ARGS__)
+#define h265d_dbg_split(fmt, ...)   h265d_dbg(H265D_DBG_SPLIT, fmt, ## __VA_ARGS__)
+#define h265d_dbg_pts(fmt, ...)     h265d_dbg(H265D_DBG_PTS, fmt, ## __VA_ARGS__)
 #define h265d_dbg_vps(fmt, ...)     h265d_dbg(H265D_DBG_VPS, fmt, ## __VA_ARGS__)
 #define h265d_dbg_sps(fmt, ...)     h265d_dbg(H265D_DBG_SPS, fmt, ## __VA_ARGS__)
 #define h265d_dbg_pps(fmt, ...)     h265d_dbg(H265D_DBG_PPS, fmt, ## __VA_ARGS__)
@@ -31,7 +35,6 @@ extern RK_U32 h265d_debug;
 #define h265d_dbg_sei(fmt, ...)     h265d_dbg(H265D_DBG_SEI, fmt, ## __VA_ARGS__)
 #define h265d_dbg_global(fmt, ...)  h265d_dbg(H265D_DBG_GLOBAL, fmt, ## __VA_ARGS__)
 #define h265d_dbg_ref(fmt, ...)     h265d_dbg(H265D_DBG_REF, fmt, ## __VA_ARGS__)
-#define h265d_dbg_time(fmt, ...)    h265d_dbg(H265D_DBG_TIME, fmt, ## __VA_ARGS__)
 #define h265d_dbg_detail(fmt, ...)  h265d_dbg(H265D_DBG_DETAIL, fmt, ## __VA_ARGS__)
 
 #endif /* H265D_DEBUG_H */
